12a9fa951df9bd91ad7d63e92d382613520ebf66
Grigoriy Serebryakov <sdrnsserver@gmail.com>
2018-04-25 16:04:26 +0300

    Common lib

---
+++ Dev/Common/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atdi.Common
+{
+    public static class DateTimeExtensions
+    {
+        public static string ConvertToISO8601DateTimeString(this DateTime value)
+        {
+            var result = new StringBuilder();
+                result.Append(value.Year.ToString("0000"));
+                result.Append("-");
+                result.Append(value.Month.ToString("00"));
+                result.Append("-");
+                result.Append(value.Day.ToString("00"));
+                result.Append("T");
+                result.Append(value.Hour.ToString("00"));
+                result.Append(":");
+                result.Append(value.Minute.ToString("00"));
+                result.Append(":");
+                result.Append(value.Second.ToString("00"));
+                result.Append("Z");
+            return result.ToString();
+        }
+    }
+}
---
+++ Dev/Common/Extensions/StringExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atdi.Common
+{
+    public static class StringExtensions
+    {
+        public static DateTime ConvertISO8601ToDateTime(this string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new System.ArgumentNullException("value");
+            }
+            return new System.DateTime(System.Convert.ToInt32(value.Substring(0, 4)), System.Convert.ToInt32(value.Substring(5, 2)), System.Convert.ToInt32(value.Substring(8, 2)), System.Convert.ToInt32(value.Substring(11, 2)), System.Convert.ToInt32(value.Substring(14, 2)), System.Convert.ToInt32(value.Substring(17, 2)), new System.Globalization.GregorianCalendar());
+        }
+
+        public static string With(this string format, params object[] args)
+        {
+            try
+            {
+                return string.Format(format, args);
+            }
+            catch
+            {
+                return format;
+            }
+        }
+
+        public static string With(this string format, IFormatProvider formatProvider, params object[] args)
+        {
+            try
+            {
+                return string.Format(formatProvider, format, args);
+            }
+            catch
+            {
+                return format;
+            }
+        }
+    }
+}
