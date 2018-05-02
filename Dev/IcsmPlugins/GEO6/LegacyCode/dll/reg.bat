ab7c1ed5488a7d225047f76ddf6067f71cb4a77b
Grigoriy Serebryakov <sdrnsserver@gmail.com>
2018-05-02 10:19:25 +0000

    Added 22 files to dll

---
+++ Dev/IcsmPlugins/GEO6/LegacyCode/dll/reg.bat
@@ -0,0 +1,14 @@
+regsvr32 "C:\Program Files\EROSoft\CTP2\CocR370.dll"
+
+tregsvr LISBCCalc.tlb
+tregsvr LISProgress.tlb
+tregsvr LISPropag.tlb
+
+regsvr32 LISBCTxServer.dll
+regsvr32 LISBCProtect.dll
+regsvr32 RSAGeography.dll
+regsvr32 p1546.dll
+regsvr32 p370.dll
+regsvr32 LISBCCalc.dll
+regsvr32 ExpServer.dll
+regsvr32 LISBCCoordDist.dll
---
+++ Dev/IcsmPlugins/GEO6/LegacyCode/dll/unreg.bat
@@ -0,0 +1,13 @@
+tregsvr -u LISBCCalc.tlb
+tregsvr -u LISProgress.tlb
+tregsvr -u LISPropag.tlb
+
+regsvr32 /u LISBCTxServer.dll
+regsvr32 /u LISBCProtect.dll
+regsvr32 /u RSAGeography.dll
+regsvr32 /u p1546.dll
+regsvr32 /u p370.dll
+regsvr32 /u LISBCCalc.dll
+regsvr32 /u ExpServer.dll
+regsvr32 /u LISBCCoordDist.dll
+
