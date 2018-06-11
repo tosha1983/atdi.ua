126a02a4c0bee77ef264bf32f55293bcaadee505
Andrey Kovpak <kovpak@outlook.com>
2018-06-11 18:17:08 +0300

    The Web Query App Service handlers was changed

--- Dev/AppServices/Atdi.AppServices.WebQuery/Handlers/GetQueryMetadataByCode.cs
+++ Dev/AppServices/Atdi.AppServices.WebQuery/Handlers/GetQueryMetadataByCode.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Atdi.Contracts.CoreServices.Identity;
 using Atdi.DataModels.Identity;
 using Atdi.DataModels.WebQuery;
 using Atdi.Platform.Logging;
@@ -12,15 +13,22 @@ namespace Atdi.AppServices.WebQuery.Handlers
 {
     public sealed class GetQueryMetadataByCode : LoggedObject
     {
-        public GetQueryMetadataByCode(ILogger logger) : base(logger)
+        private readonly QueriesRepository _repository;
+        private readonly IUserTokenProvider _tokenProvider;
+
+        public GetQueryMetadataByCode(QueriesRepository repository, IUserTokenProvider tokenProvider, ILogger logger) : base(logger)
         {
+            this._repository = repository;
+            this._tokenProvider = tokenProvider;
         }
 
         public QueryMetadata Handle(UserToken userToken, string queryCode)
         {
             using (this.Logger.StartTrace(Contexts.WebQueryAppServices, Categories.Handling, TraceScopeNames.GetQueryMetadataByCode))
             {
-                return new QueryMetadata() { Token = new QueryToken { Id = 0, Version = "1111.44" } };
+                var tokenData = this._tokenProvider.UnpackUserToken(userToken);
+                var queryDescriptor = this._repository.GetQueryDescriptorByCode(tokenData, queryCode);
+                return queryDescriptor.Metadata;
             }
         }
     }
