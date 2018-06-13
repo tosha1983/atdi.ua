2e887b10ead29bb704540a219f2ff7b77d4c6a13
LIS <ProgramSilver@gmail.com>
2018-06-13 10:02:25 +0300

    Correct Plugin WebQuery

--- Dev/IcsmPlugins/Plugin WebQuery/Forms/ClassORM.cs
+++ Dev/IcsmPlugins/Plugin WebQuery/Forms/ClassORM.cs
@@ -19,9 +19,9 @@ namespace XICSM.WebQuery
 
         public static List<string> GetProperties(string Query, bool isDecrypt)
         {
-            List<string> str_fld = new List<string>();
-            Class_IRP_Object hg_ = new Class_IRP_Object();
-            IcsmReport ics = new IcsmReport();
+            var str_fld = new List<string>();
+            var hg_ = new Class_IRP_Object();
+            var ics = new IcsmReport();
             Frame f = new Frame();
             int x1 = Query.IndexOf("\r\n");
             Query = Query.Remove(0, x1 + 2);
@@ -31,14 +31,14 @@ namespace XICSM.WebQuery
             f.Load(strx);
             ics.SetConfig(f);
             if (ics != null)   {
-                hg_.TABLE_NAME = ics.m_dat.m_tab;
+                hg_.tablename = ics.m_dat.m_tab;
                 if (ics.m_dat.m_list.Count() > 0)    {
                     for (int i = 0; i < ics.m_dat.m_list[0].m_query.lq.Count(); i++)    {
                         if (!ics.m_dat.m_list[0].m_query.lq[i].m_isCustExpr)   {
                             string t = ics.m_dat.m_list[0].m_query.lq[i].path;
                             t = t.Replace(ics.m_dat.m_tab + ".", "");
-                            hg_.FLD.Add(t);
-                            hg_.CAPTION_FLD.Add(ics.m_dat.m_list[0].m_query.lq[i].title);
+                            hg_.fld.Add(t);
+                            hg_.captionfld.Add(ics.m_dat.m_list[0].m_query.lq[i].title);
                             str_fld.Add(t);
                         }
                         else {
@@ -47,8 +47,8 @@ namespace XICSM.WebQuery
                             nw_.m_name = ics.m_dat.m_list[0].m_query.lq[i].title;
                             nw_.m_sp = ics.m_dat.m_list[0].m_query.lq[i].m_typeCustExpr;
                             str_fld.Add(nw_.m_name);
-                            hg_.FLD.Add(nw_.m_name);
-                            hg_.CAPTION_FLD.Add(nw_.m_name);
+                            hg_.fld.Add(nw_.m_name);
+                            hg_.captionfld.Add(nw_.m_name);
                         }
                     }
                 }
@@ -62,18 +62,18 @@ namespace XICSM.WebQuery
 
     public class Class_IRP_Object
     { 
-        public List<string> FLD { get; set; }
-        public List<Type> FLD_TYPE { get; set; }
-        public List<string> CAPTION_FLD { get; set; }
-        public string FILTER { get; set; }
-        public string TABLE_NAME { get; set; }
+        public List<string> fld { get; set; }
+        public List<Type> fldtype { get; set; }
+        public List<string> captionfld { get; set; }
+        public string filter { get; set; }
+        public string tablename { get; set; }
 
 
         public Class_IRP_Object()
         {
-            FLD = new List<string>();
-            CAPTION_FLD = new List<string>();
-            FLD_TYPE = new List<Type>();
+            fld = new List<string>();
+            captionfld = new List<string>();
+            fldtype = new List<Type>();
         }
 
 
@@ -85,10 +85,6 @@ namespace XICSM.WebQuery
 
         public void Dispose()
         {
-            FLD = null;
-            CAPTION_FLD = null;
-            FILTER = "";
-            TABLE_NAME = "";
             GC.SuppressFinalize(this);
         }
 
