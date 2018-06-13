1274289725211d1eb7cf1bb72863f3ad2bb89e89
LIS <ProgramSilver@gmail.com>
2018-06-13 09:58:22 +0300

    Correct Plugin WebQuery

--- Dev/IcsmPlugins/Plugin WebQuery/TablesName.cs
+++ Dev/IcsmPlugins/Plugin WebQuery/TablesName.cs
@@ -2,110 +2,15 @@
 {
     //=========================================================
     /// <summary>
-    /// Название таблиц ICSM
+    /// Название таблиц 
     /// </summary>
     public static class ICSMTbl
     {
         public const string WebQuery = "XWEBQUERY";
         public const string WebConstraint = "XWEBCONSTRAINT";
-        public const string Tarif = "TARIFF";
-        public const string Invoices = "INVOICES";
-        public const string Invoice_item = "INVOICE_ELT";
-        public const string lnkSites = "XNRFA_LINKS_SITES";
-        public const string itblFmtvAssign = "FMTV_ASSIGN";
-        public const string itblSatellites = "SATELLITES";
-        public const string EQUIP_BRO = "EQUIP_BRO";
-        public const string itblReminder = "REMINDER";
-        public const string itblReminderTrg = "REMINDER_TRG";
-        public const string itblEquipBro = EQUIP_BRO;
-        public const string itblEquipMob2 = "EQUIP_MOB2";
-        public const string itblEquipEsta = "EQUIP_ESTA";
-        public const string itblAntenna = "ANTENNA";
-        public const string ANTENNA_BRO = "ANTENNA_BRO";
-        public const string itblAntennaBro = ANTENNA_BRO;
-        public const string AntennaMW = "ANTENNA_MW";
-        public const string itblEstaAntenna = "ESTA_ANTENNA";
-        public const string itblEstaGroup = "ESTA_GROUP";
-        public const string itblEstaAssgn = "ESTA_ASSGN";
-        public const string itblEstaEmiss = "ESTA_EMISS";
-        public const string itblAntennaMob2 = "ANTENNA_MOB2";
-        public const string itblAntennaMob = "ANTENNA_MOB";
         public const string USERS = "USERS";
         public const string itblUsers = USERS;
         public const string itblUsersContacts = "USERS_CNTCT";
-
-        public const string itblEarthStation = "EARTH_STATION";
-        public const string MobStation2 = "MOB_STATION2";
-        public const string itblMobStation2 = MobStation2;
-        public const string itblMobStation = "MOB_STATION";
-        public const string itblBgMobStation = "BG_MOB_STATION";
-        public const string itblEquipPmrMpt = "EQUIP_PMR_MPT";
-        public const string itblEquipPmr = "EQUIP_PMR";
-        public const string POSITION_ES = "POSITION_ES";
-        public const string itblPositionEs = POSITION_ES;
-        public const string itblPositionBro = "POSITION_BRO";
-        public const string itblPositionFmn = "POSITION_FMN";
-        public const string itblPositionHf = "POSITION_HF";
-        public const string itblPositionMob2 = "POSITION_MOB2";
-        public const string itblPositionMw = "POSITION_MW";
-        public const string itblPositionWim = "POSITION_WIM";
-        public const string itblAdmSite = "SITES";
-        public const string Site = "SITE";
-        public const string itblSite = Site;        
-        public const string itblSites = "SITES";
-        public const string SITES = itblSites;
-        public const string itblMicrows = "MICROWS";
-        public const string itblMicrowa = "MICROWA";
-        public const string EQUIP_MW = "EQUIP_MW";
-        public const string itblEquipMicrow = EQUIP_MW;
-        public const string itblMicrowAntenna = "ANTENNA_MW";
-        public const string DocFiles = "DOCFILES";
-        public const string itblDocLink = "DOCLINK";
-        public const string itblSysConfig = "SYS_CONFIG";
-        public const string itblEmployee = "EMPLOYEE";
-        public const string itblArea = "AREA";
-        public const string itblCities = "CITIES";
-        public const string itblCombo = "COMBO";
-        public const string itblAhAllotments = "CH_ALLOTMENTS";
-        public const string itblChAllotCh = "CH_ALLOTED_CH";
-        public const string ChAllotments = itblAhAllotments;
-        public const string LICENCE = "LICENCE";
-        public const string itblLicence = LICENCE;
-        public const string itblMobStaFreqs2 = "MOBSTA_FREQS2";
-        public const string itblMobStaFreqs = "MOBSTA_FREQS";
-        public const string itblTV = "TV_STATION";
-        public const string itblBGTV = "BG_TV_STATION";
-        public const string itblFM = "FM_STATION";
-        public const string itblBGFM = "BG_FM_STATION";
-        public const string itblDVBT = "DVBT_STATION";
-        public const string itblBGDVBT = "BG_DVBT_STATION";
-        public const string itblTDAB = "TDAB_STATION";
-        public const string itblBGTDAB = "BG_TDAB_STATION";
-        public const string itblFreqPlan = "FREQ_PLAN";
-        public const string itblFreqPlanChan = "FREQ_PLAN_CHAN";
-        public const string itblGSM = "GSM";
-        public const string itblShip = "SHIP";
-        public const string itblShipEqp = "SHIP_EQP";
-        public const string itblRadioSystem = "RADIO_SYSTEMS";
-        public const string itblItuContour = "ITU_CONTOUR";
-        public const string itblTskfMember = "TSKF_MEMBER";
-        
-        public const string FxLkClink = "FXLK_CLINK";
-        public const string FxLkClinkSta = "FXLK_CLINKSTA";
-        public const string FxLkClinkPrv = "FXLK_CLINKPRV";
-        public const string FxLkCoordReg = "FXLK_COORD_REQ";
-
-        public const string HcmFixlink = "HCMFIX_LINK";
-        public const string HcmFixSta = "HCMFIX_STA";
-        
-        public const string WienCoordMob = "WIEN_COORD_MOB";
-        public const string WienAnsMob = "WIEN_ANS_MOB";
-        public const string FixMobNotice = "FIXMOBNOTICE";
-        public const string T1nAntennas = "T1N_ANTENNAS";
-        public const string T1nRxTxStations = "T1N_RXTX_STATIONS";
-        public const string T1nRotational = "T1N_ROTATIONAL";
-        //--
-        public const string AllPositions = "ALL_POSITIONS";
     }
   
 }
