8b53355e7b63b7959fc1df5fda0ae02b4db68860
Anton Kharkhardin <Anton.Kharkhardin@outlook.com>
2018-09-03 16:46:49 +0300

    Added New Meas Task Form

---
+++ Dev/IcsmPlugins/ICS Control Client/Handlers/Tour/Commands/SelectAreaCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ICSM;
+using XICSM.ICSControlClient.Environment;
+using DM = XICSM.ICSControlClient.Models.SynchroInspections;
+
+namespace XICSM.ICSControlClient.Handlers.TourCommnads
+{
+    public class SelectAreaCommand
+    {
+        public static bool Handle(IMQueryMenuNode.Context context)
+        {
+            return
+                context.ExecuteContextMenuAction(
+                        PluginMetadata.Processes.SelectArea,
+                        SelectAreaForTour
+                    );
+        }
+        private static bool SelectAreaForTour(int tourId)
+        {
+            //var tour = Repository.ReadEntityById<DM.Tour>(tourId);
+
+            //tour.
+            //Repository.UpdateEntity(tour);
+
+
+            System.Windows.Forms.MessageBox.Show("Select Area for tour with ID - " + tourId + " : ", "SelectAreaCommand", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Exclamation);
+            return true;
+        }
+    }
+}
