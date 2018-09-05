a5f348f5293e470ef452c010e58027756553c0fc
Anton Kharkhardin <Anton.Kharkhardin@outlook.com>
2018-09-05 16:53:25 +0300

    Update command "Select Area for tour"

--- Dev/IcsmPlugins/ICS Control Client/Forms/SelectAreaForm.Designer.cs
+++ Dev/IcsmPlugins/ICS Control Client/Forms/SelectAreaForm.Designer.cs
@@ -28,37 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.icsDBList1 = new NetPlugins2.IcsDBList();
+            this.icsDBList_Areas = new NetPlugins2.IcsDBList();
+            this.btnSelect = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // icsDBList1
+            // icsDBList_Areas
             // 
-            this.icsDBList1.BackColor = System.Drawing.SystemColors.Control;
-            this.icsDBList1.ConfigName = null;
-            this.icsDBList1.Filter = null;
-            this.icsDBList1.Location = new System.Drawing.Point(0, 0);
-            this.icsDBList1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.icsDBList1.Name = "icsDBList1";
-            this.icsDBList1.Param1 = 0;
-            this.icsDBList1.Param2 = 0;
-            this.icsDBList1.Size = new System.Drawing.Size(789, 451);
-            this.icsDBList1.TabIndex = 0;
-            this.icsDBList1.Table = null;
+            this.icsDBList_Areas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.icsDBList_Areas.BackColor = System.Drawing.SystemColors.Control;
+            this.icsDBList_Areas.ConfigName = null;
+            this.icsDBList_Areas.Filter = null;
+            this.icsDBList_Areas.Location = new System.Drawing.Point(11, 11);
+            this.icsDBList_Areas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.icsDBList_Areas.Name = "icsDBList_Areas";
+            this.icsDBList_Areas.Param1 = 0;
+            this.icsDBList_Areas.Param2 = 0;
+            this.icsDBList_Areas.Size = new System.Drawing.Size(1451, 519);
+            this.icsDBList_Areas.TabIndex = 0;
+            this.icsDBList_Areas.Table = null;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelect.Location = new System.Drawing.Point(1209, 554);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(220, 35);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
             // 
             // SelectAreaForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.icsDBList1);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1473, 607);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.icsDBList_Areas);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "SelectAreaForm";
             this.Text = "SelectArea";
+            this.Load += new System.EventHandler(this.SelectAreaForm_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private NetPlugins2.IcsDBList icsDBList1;
+        private NetPlugins2.IcsDBList icsDBList_Areas;
+        private System.Windows.Forms.Button btnSelect;
     }
 }
--- Dev/IcsmPlugins/ICS Control Client/Forms/SelectAreaForm.cs
+++ Dev/IcsmPlugins/ICS Control Client/Forms/SelectAreaForm.cs
@@ -7,14 +7,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ICSM;
+//using XICSM.ICSControlClient.Environment;
+//using DM = XICSM.ICSControlClient.Models.SynchroInspections;
 
 namespace XICSM.ICSControlClient.Forms
 {
     public partial class SelectAreaForm : Form
     {
+        public int tourId;
         public SelectAreaForm()
         {
             InitializeComponent();
+        }
+        private void SelectAreaForm_Load(object sender, EventArgs e)
+        {
+            icsDBList_Areas.Init();
+            icsDBList_Areas.AddColumn("AREA.ID", "ID", "Auto", 48);
+            icsDBList_Areas.AddColumn("AREA.NAME", "Area Name", "Auto", 354);
+            icsDBList_Areas.AddColumn("AREA.DENSITY", "Type_of_the_area", "Auto", 142);
+            icsDBList_Areas.AddColumn("AREA.FAMILY", "Family_of_the_area", "Auto", 195);
+            icsDBList_Areas.AddColumn("AREA.DistanceToPoint", "Distance to the reference point", "Auto", 142);
+            icsDBList_Areas.AddColumn("AREA.LONGITUDE", "Longitude (Deg)", "Auto", 159);
+            icsDBList_Areas.AddColumn("AREA.LATITUDE", "Latitude (Deg)", "Auto", 159);
+            icsDBList_Areas.AddColumn("AREA.COUNTRY", "Country", "Auto", 51);
+            icsDBList_Areas.AddColumn("AREA.DATE_CREATED", "DATE_CREATED", "Auto", 177);
+            icsDBList_Areas.AddColumn("AREA.POINTS", "List of vertices for the polygon description", "Auto", 354);
+            icsDBList_Areas.Table = "AREA";
+            icsDBList_Areas.Requery();
+
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            string points = "";
+            if (icsDBList_Areas.GetCurrentLineData("POINTS", ref points))
+            {
+                var source = new IMRecordset("INSP_TOUR", IMRecordset.Mode.ReadWrite);
+                source.Select("ID,CUST_TXT9");
+                source.SetWhere("ID", IMRecordset.Operation.Eq, tourId.ToString());
+                using (source.OpenWithScope())
+                {
+                    if (source.IsEOF())
+                    {
+                        throw new InvalidOperationException($"Not found a record of INSP_TOUR by Id #{tourId.ToString()}");
+                    }
+                    source.Edit();
+                    source.Put("CUST_TXT9", ConvertPoints(points));
+                    source.Update();
+                }
+            }
+            this.Close();
+        }
+        private string ConvertPoints(string inputPoints)
+        {
+            string outputPoints = "";
+            if (string.IsNullOrEmpty(inputPoints))
+                return outputPoints;
+
+            foreach (var a in inputPoints.Split(new[] { "\r\n" }, StringSplitOptions.None))
+            {
+                if (!string.IsNullOrEmpty(a))
+                {
+                    string[] b = a.Split(new[] { "\t" }, StringSplitOptions.None);
+                    if (b.Length == 2)
+                    {
+                        if (string.IsNullOrEmpty(outputPoints))
+                            outputPoints = string.Join(",", new[] { b[1], b[0] });
+                        else
+                            outputPoints = outputPoints + "," + string.Join(",", new[] { b[1], b[0] });
+                    }
+                }
+            }
+            return outputPoints;
         }
     }
 }
