4732a6025b422b6a947dcfe2452d928441c1d5cd
Anton Kharkhardin <Anton.Kharkhardin@outlook.com>
2018-09-06 16:32:59 +0300

    Some changes in command Select Area

--- Dev/IcsmPlugins/ICS Control Client/Forms/SelectAreaForm.cs
+++ Dev/IcsmPlugins/ICS Control Client/Forms/SelectAreaForm.cs
@@ -8,7 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ICSM;
-//using XICSM.ICSControlClient.Environment;
 //using DM = XICSM.ICSControlClient.Models.SynchroInspections;
 
 namespace XICSM.ICSControlClient.Forms
@@ -30,6 +29,7 @@ namespace XICSM.ICSControlClient.Forms
             icsDBList_Areas.AddColumn("AREA.DistanceToPoint", "Distance to the reference point", "Auto", 142);
             icsDBList_Areas.AddColumn("AREA.LONGITUDE", "Longitude (Deg)", "Auto", 159);
             icsDBList_Areas.AddColumn("AREA.LATITUDE", "Latitude (Deg)", "Auto", 159);
+            icsDBList_Areas.AddColumn("AREA.CSYS", "Coordinate system ", "Auto", 159);
             icsDBList_Areas.AddColumn("AREA.COUNTRY", "Country", "Auto", 51);
             icsDBList_Areas.AddColumn("AREA.DATE_CREATED", "DATE_CREATED", "Auto", 177);
             icsDBList_Areas.AddColumn("AREA.POINTS", "List of vertices for the polygon description", "Auto", 354);
@@ -41,8 +41,16 @@ namespace XICSM.ICSControlClient.Forms
         private void btnSelect_Click(object sender, EventArgs e)
         {
             string points = "";
+            string csys = "";
+
             if (icsDBList_Areas.GetCurrentLineData("POINTS", ref points))
             {
+                if (!icsDBList_Areas.GetCurrentLineData("CSYS", ref csys) || ((!"4DEC".Equals(csys, StringComparison.OrdinalIgnoreCase)) && (!"4DMS".Equals(csys, StringComparison.OrdinalIgnoreCase))))
+                {
+                    MessageBox.Show("Polygon mast have coordinate system 4DEC or 4DMS", "SelectAreaCommand", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+
                 var source = new IMRecordset("INSP_TOUR", IMRecordset.Mode.ReadWrite);
                 source.Select("ID,CUST_TXT9");
                 source.SetWhere("ID", IMRecordset.Operation.Eq, tourId.ToString());
@@ -52,14 +60,16 @@ namespace XICSM.ICSControlClient.Forms
                     {
                         throw new InvalidOperationException($"Not found a record of INSP_TOUR by Id #{tourId.ToString()}");
                     }
+
+                    var newpoints = ConvertPoints(points, csys);
                     source.Edit();
-                    source.Put("CUST_TXT9", ConvertPoints(points));
+                    source.Put("CUST_TXT9", newpoints);
                     source.Update();
                 }
             }
             this.Close();
         }
-        private string ConvertPoints(string inputPoints)
+        private string ConvertPoints(string inputPoints, string csys)
         {
             string outputPoints = "";
             if (string.IsNullOrEmpty(inputPoints))
@@ -72,10 +82,27 @@ namespace XICSM.ICSControlClient.Forms
                     string[] b = a.Split(new[] { "\t" }, StringSplitOptions.None);
                     if (b.Length == 2)
                     {
-                        if (string.IsNullOrEmpty(outputPoints))
-                            outputPoints = string.Join(",", new[] { b[1], b[0] });
+                        string coord1 = "";
+                        string coord2 = "";
+                        if ("4DMS".Equals(csys, StringComparison.OrdinalIgnoreCase))
+                        {
+                            double k1;
+                            double k2;
+                            if (double.TryParse(b[0].Replace('.', ','), out k1) && double.TryParse(b[1].Replace('.', ','), out k2))
+                            {
+                                coord1 = IMPosition.Dms2Dec(k1).ToString().Replace(',', '.');
+                                coord2 = IMPosition.Dms2Dec(k2).ToString().Replace(',', '.');
+                            }
+                        }
                         else
-                            outputPoints = outputPoints + "," + string.Join(",", new[] { b[1], b[0] });
+                        {
+                            coord1 = b[0];
+                            coord2 = b[1];
+                        }
+                        if (string.IsNullOrEmpty(outputPoints))
+                            outputPoints = string.Join(",", new[] { coord2, coord1 });
+                        else
+                            outputPoints = outputPoints + "," + string.Join(",", new[] { coord2, coord1 });
                     }
                 }
             }
