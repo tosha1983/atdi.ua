affc79f20b543408e17b256626ab8e9c3062f503
Oleg Kozlov <oleg_89@meta.ua>
2018-09-08 16:45:44 +0300

    Summary updates (for API description)

--- Dev/DataModels/Atdi.DataModels.Sdrns.Device/Entities/Entity.cs
+++ Dev/DataModels/Atdi.DataModels.Sdrns.Device/Entities/Entity.cs
@@ -11,73 +11,73 @@ namespace Atdi.DataModels.Sdrns.Device
     public class Entity 
     {
         /// <summary>
-        /// 
+        /// Entity identifier
         /// </summary>
         [DataMember]
         public string EntityId { get; set; }
 
         /// <summary>
-        /// 
+        /// Entity name
         /// </summary>
         [DataMember]
         public string Name { get; set; }
 
         /// <summary>
-        /// 
+        /// Entity description
         /// </summary>
         [DataMember]
         public string Description { get; set; }
 
         /// <summary>
-        /// 
+        /// Entity parent identifier
         /// </summary>
         [DataMember]
         public string ParentId { get; set; }
 
         /// <summary>
-        /// 
+        /// Entity parent type
         /// </summary>
         [DataMember]
         public string ParentType { get; set; }
 
         /// <summary>
-        /// 
+        /// Entity content type
         /// </summary>
         [DataMember]
         public string ContentType { get; set; }
 
         /// <summary>
-        /// 
+        /// Entity encoding
         /// </summary>
         [DataMember]
         public string Encoding { get; set; }
 
         /// <summary>
-        /// 
+        /// Entity hash algorithm
         /// </summary>
         [DataMember]
         public string HashAlgorithm { get; set; }
 
         /// <summary>
-        /// 
+        /// Entity hash code
         /// </summary>
         [DataMember]
         public string HashCode { get; set; }
 
         /// <summary>
-        /// 
+        /// Entity part index
         /// </summary>
         [DataMember]
         public int PartIndex { get; set; }
 
         /// <summary>
-        /// 
+        /// Defines the Entity EOF
         /// </summary>
         [DataMember]
         public bool EOF { get; set; }
 
         /// <summary>
-        /// 
+        /// Entity content
         /// </summary>
         [DataMember]
         public byte[] Content { get; set; }
