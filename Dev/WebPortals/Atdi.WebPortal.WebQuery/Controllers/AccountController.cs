09567739b976c5004ad4185951939c0ef8ba8277
Andrey Kovpak <kovpak@outlook.com>
2018-10-25 18:38:34 +0300

    Added Web Query web portal project

---
+++ Dev/WebPortals/Atdi.WebPortal.WebQuery/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Atdi.WebPortal.WebQuery.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Atdi.WebPortal.WebQuery.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/SignIn
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult SignIn(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        //
+        // POST: /Account/SignIn
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignIn(SignInViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return RedirectToLocal(returnUrl);
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            return RedirectToAction("Index", "Portal");
+        }
+    }
+}
