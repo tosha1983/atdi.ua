c4e3bb25bafc9d262719917dc129bf4a58135f19
LIS <ProgramSilver@gmail.com>
2018-11-06 08:43:20 +0200

    Added UnitTest for EntityOrm

--- Dev/UnitTesting/CoreServices/Atdi.UnitTest.CoreServices.EntityOrm/EntityOrmUnitTest.cs
+++ Dev/UnitTesting/CoreServices/Atdi.UnitTest.CoreServices.EntityOrm/EntityOrmUnitTest.cs
@@ -10,15 +10,20 @@ namespace Atdi.UnitTest.CoreServices.EntityOrm
         [TestMethod]
         public void Test_CheckCreation()
         {
+            // Конечный путь к файлам xml формируется следующим образом:
+            // директория в которой расположен файл Environment.xml \ RootPath \ Поддиректории 
+            string ExeDir = @"c:\projects\reposApi2\Dev\UnitTesting\CoreServices\Atdi.UnitTest.CoreServices.EntityOrm\bin\Debug\Metadata";
             var config = new EntityOrmConfigFake()
             {
-                RootPath ="",
-                DataTypesPath = ""
+                RootPath = ExeDir + "\\",
+                DataTypesPath = ExeDir + "\\DataTypes",
+                EntitiesPath = ExeDir + "\\Entities",
+                UnitsPath = ExeDir + "\\Units"
             };
-
             var orm = new TST.EntityOrm(config);
-
-            
+            Contracts.CoreServices.EntityOrm.Metadata.IDataTypeMetadata dataTypeMetadata = orm.GetDataTypeMetadata("Int64", Contracts.CoreServices.EntityOrm.Metadata.DataSourceType.Database);
+            Contracts.CoreServices.EntityOrm.Metadata.IEntityMetadata entityMetadata = orm.GetEntityMetadata("Sensor");
+            Contracts.CoreServices.EntityOrm.Metadata.IUnitMetadata unitMetadata = orm.GetUnitMetadata("Frequency.MHz.xml");
         }
     }
 }
