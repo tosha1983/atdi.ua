132cfc2e8f790ba76b844d7f6a2d5f9ad1dad036
Jerzy Syniepolski <jws1412@gmail.com>
2018-11-28 13:54:12 +0200

    Check Plugin WebQuery

--- Dev/IcsmPlugins/Plugin WebQuery/UpdateSchema.cs
+++ Dev/IcsmPlugins/Plugin WebQuery/UpdateSchema.cs
@@ -70,7 +70,7 @@ namespace XICSM.Atdi.Icsm.Plugins.WebQuery
                 s.DeclareField("TABLECOLUMNS", "VARCHAR(4000)", null, null, null);
             }
 
-            s.DeclareTable("XWEBQUERYATTRIBUTES", "Web constraints", plugin4);
+            s.DeclareTable("XWEBQUERYATTRIBUTES", "Web attributes", plugin4);
             {
                 s.DeclareField("ID", "NUMBER(9,0)", null, "NOTNULL", null);
                 s.DeclareIndex("PK_XWEBQUERYATTRIBUTES", "PRIMARY", "ID");
@@ -133,16 +133,17 @@ namespace XICSM.Atdi.Icsm.Plugins.WebQuery
         /// <returns>true</returns>
         public static bool UpgradeDatabase(IMSchema s, double dbCurVersion)
         {
-            if (dbCurVersion < 20180105.0949)
+            if (dbCurVersion < 20181128.0949)
             {
-                //s.CreateTables("XWEBQUERY,XWEBCONSTRAINT,XUPDATEOBJECTS");
-                //s.CreateTableFields("XWEBCONSTRAINT", "ID,WEBQUERYID,NAME,PATH,MIN,MAX,STRVALUE,DATEVALUEMIN,INCLUDE,DATEVALUEMAX");
-                //s.CreateTableFields("XWEBQUERY", "ID,NAME,QUERY,COMMENTS,IDENTUSER,CODE,TASKFORCEGROUP");
+                s.CreateTables("XWEBQUERY,XWEBCONSTRAINT,XUPDATEOBJECTS,XWEBQUERYATTRIBUTES");
+                s.CreateTableFields("XWEBCONSTRAINT", "ID,WEBQUERYID,NAME,PATH,MIN,MAX,STRVALUE,DATEVALUEMIN,INCLUDE,DATEVALUEMAX");
+                s.CreateTableFields("XWEBQUERY", "ID,NAME,QUERY,COMMENTS,IDENTUSER,CODE,TASKFORCEGROUP,VIEWCOLUMNS,ADDCOLUMNS,EDITCOLUMNS,TABLECOLUMNS");
+                s.CreateTableFields("XWEBQUERYATTRIBUTES", "ID,WEBQUERYID,PATH,READONLY,NOTCHANGEADD,NOTCHANGEEDIT");
                 //s.CreateTables("XWEBTEST");
                 //s.CreateTableFields("XUPDATEOBJECTS", "ID,OBJTABLE,DATEMODIFIED");
                 //s.CreateTables("XWEBTEST");
                 //s.CreateTableFields("XWEBTEST", "ID,STRING_TYPE,BOOLEAN_TYPE,INTEGER_TYPE,DATETIME_TYPE,DOUBLE_TYPE,FLOAT_TYPE,DECIMAL_TYPE,BYTE_TYPE,BYTES_TYPE,GUID_TYPE");
-                s.SetDatabaseVersion(20180105.0949);
+                s.SetDatabaseVersion(20181128.0949);
             }
             return true;
         }
@@ -151,6 +152,7 @@ namespace XICSM.Atdi.Icsm.Plugins.WebQuery
         /// <summary>
         /// Текущая версия БД плагина
         /// </summary>
-        public static readonly double schemaVersion = 20180105.0949;//20161003.0909
+        //public static readonly double schemaVersion = 20180105.0949;//20161003.0909
+        public static readonly double schemaVersion = 20181128.0949;//20161003.0909
     }
 }
