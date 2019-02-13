9862f14cfbb27df07eb7123af565ce3a50182287
maxkolomytsev <maxkolomytsev@gmail.com>
2019-02-13 11:21:59 +0200

    Add data models for adapter for IQ stream

---
+++ Dev/DataModels/Atdi.DataModels.Sdrn.DeviceServer.Commands/Parameters/MesureIQStreamParameter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atdi.DataModels.Sdrn.DeviceServer.Commands.Parameters
+{
+    public class MesureIQStreamParameter
+    {
+        public decimal FreqStart_Hz; // mandatory 
+        public decimal FreqStop_Hz;  // mandatory 
+        public int Att_dB; //-1 = auto, 
+        public int PreAmp_dB; //-1 = auto, 
+        public int RefLevel_dBm; // -1 = auto
+        double BitRate_MBs; // скорость потока IQ;  -1 = максимальная
+        double IQBlockDuration_s; // длительность ожидаемого блока с IQ
+        double IQReceivTime_s; // общее временное окно отводимое для получения потока IQ
+        bool MandatoryPPS; // обязательная привязка к PPS 
+        bool MandatorySignal; //  Снятие потока только при наличии наличия сигнала
+        long TimeStart; // Время старта записи IQ stream
+    }
+}
+
---
+++ Dev/DataModels/Atdi.DataModels.Sdrn.DeviceServer.Commands/Results/MesureIQStreamResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atdi.DataModels.Sdrn.DeviceServer.Commands.Results
+{
+    public class MesureStreamResult : CommandResultPartBase
+    {
+        public MesureStreamResult(ulong partIndex, CommandResultStatus status)
+               : base(partIndex, status)
+        {
+        }
+        public float[][] iq_samples;
+        public long OneSempleDuration_ns;
+        public long TimeStamp; //Тики относительно new DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc)
+        public long PPSTimeDifference_ns; //PPS относительно ближайшей секунды TimeStamp;
+    }
+}
