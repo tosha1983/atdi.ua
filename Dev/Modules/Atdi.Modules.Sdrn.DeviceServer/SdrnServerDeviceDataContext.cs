c9846ebe0138416d5946531a74d94644b85d690f
Andrey Kovpak <kovpak@outlook.com>
2019-02-19 15:16:43 +0200

    Updated environment SDRN.DeviceServer

---
+++ Dev/Modules/Atdi.Modules.Sdrn.DeviceServer/SdrnServerDeviceDataContext.cs
@@ -0,0 +1,17 @@
+﻿using Atdi.Contracts.CoreServices.DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atdi.Modules.Sdrn.DeviceServer
+{
+    public class SdrnServerDeviceDataContext : DataContextBase
+    {
+        public SdrnServerDeviceDataContext() 
+            : base("SDRN_DeviceServer_DB")
+        {
+        }
+    }
+}
