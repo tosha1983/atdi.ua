d821cb520e0659668e3a795948cbf5eea5ac1f4c
maxkolomytsev <maxkolomytsev@gmail.com>
2019-02-20 16:01:26 +0200

    Add the class for task and process (SDRN Device)

--- Dev/DataModels/Atdi.DataModels.Sdrn.DeviceServer.Processing/Tasks/SOTask.cs
+++ Dev/DataModels/Atdi.DataModels.Sdrn.DeviceServer.Processing/Tasks/SOTask.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Atdi.DataModels.Sdrns.Device;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,7 @@ using System.Threading.Tasks;
 
 namespace Atdi.DataModels.Sdrn.DeviceServer.Processing
 {
-    public class ExampleProcess : ProcessBase
+    public class SOTask : TaskBase
     {
-        public int TaskId;
-
-        public ExampleProcess() : base("Example process")
-        {
-        }
     }
 }
