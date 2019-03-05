46fcc94ade000b09a974b2dc575b9ee57349e5a1
Евгений Ковалевский <jacks.des@gmail.com>
2019-03-05 17:20:56 +0200

    Merge

--- Dev/Test/Atdi.Test.Sdrn.DeviceServer.Adapters.WPF/MainWindow.xaml.cs
+++ Dev/Test/Atdi.Test.Sdrn.DeviceServer.Adapters.WPF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AC = Atdi.Common;
 using Atdi.Contracts.Sdrn.DeviceServer;
 using ADP = Atdi.AppUnits.Sdrn.DeviceServer.Adapters;
 using CMD = Atdi.DataModels.Sdrn.DeviceServer.Commands;
@@ -28,6 +29,7 @@ namespace Atdi.Test.Sdrn.DeviceServer.Adapters.WPF
     public partial class MainWindow : Window
     {
         // подготовка тестового окружения
+        DummyTimeService TimeService;
         ConsoleLogger logger;
         DummyAdapterHost adapterHost;
         ADP.SignalHound.Adapter adapter;
@@ -37,8 +39,8 @@ namespace Atdi.Test.Sdrn.DeviceServer.Adapters.WPF
             InitializeComponent();
             logger = new ConsoleLogger();
             adapterHost = new DummyAdapterHost(logger);
+            TimeService = new DummyTimeService();
 
-            
             // конфигурация
             var adapterConfig = new ADP.SignalHound.AdapterConfig()
             {
@@ -50,6 +52,7 @@ namespace Atdi.Test.Sdrn.DeviceServer.Adapters.WPF
             };
             
             adapter = new ADP.SignalHound.Adapter(adapterConfig, logger);
+            //adapter = new ADP.SignalHound.Adapter(adapterConfig, logger, TimeService);
             DS.Adapter = adapter;
 
             
@@ -73,8 +76,8 @@ namespace Atdi.Test.Sdrn.DeviceServer.Adapters.WPF
             command.Parameter.FreqStart_Hz = 90000000;
             command.Parameter.FreqStop_Hz = 110000000;
             command.Parameter.PreAmp_dB = 20;
-            command.Parameter.RBW_Hz = 30000;
-            command.Parameter.VBW_Hz = 30000;
+            command.Parameter.RBW_Hz = -1;
+            command.Parameter.VBW_Hz = -1;
             command.Parameter.RefLevel_dBm = -40;
             command.Parameter.SweepTime_s = 0.00001;
             command.Parameter.TraceCount = 10;
@@ -180,6 +183,33 @@ namespace Atdi.Test.Sdrn.DeviceServer.Adapters.WPF
             command.Parameter.TimeStart += 2*10000000;
             Debug.WriteLine("\r\n" + new TimeSpan(command.Parameter.TimeStart).ToString() + " Set Param");
             adapter.MesureIQStreamCommandHandler(command, context);
+        }
+
+
+        
+    }
+    public class DummyTimeService : ITimeService
+    {
+        private readonly ITimeStamp _timeStamp;
+
+        public DummyTimeService()
+        {
+            this._timeStamp = new TimeStamp();
+        }
+
+        public ITimeStamp TimeStamp => this._timeStamp;
+    }
+    class TimeStamp : ITimeStamp
+    {
+        public long Milliseconds => AC.TimeStamp.Milliseconds;
+
+        public long Value => AC.TimeStamp.Value;
+
+        public long Ticks => AC.TimeStamp.Ticks;
+
+        public bool HitMilliseconds(long startStampMilliseconds, long timeoutMilliseconds)
+        {
+            return AC.TimeStamp.HitTimeout(startStampMilliseconds, timeoutMilliseconds);
         }
     }
 }
