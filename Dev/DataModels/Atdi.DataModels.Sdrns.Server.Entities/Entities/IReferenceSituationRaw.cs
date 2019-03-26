2b04b8e98489c89c3b58a9d2454f2d761c3391cb
LIS <ProgramSilver@gmail.com>
2019-03-26 12:49:25 +0200

    Add new xml to Atdi.DataModels.Sdrns.Server.Entities. Check Primary handlers and WcfServices Sdrn.

---
+++ Dev/DataModels/Atdi.DataModels.Sdrns.Server.Entities/Entities/IReferenceSituationRaw.cs
@@ -0,0 +1,20 @@
+﻿using Atdi.DataModels.EntityOrm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atdi.DataModels.Sdrns.Server.Entities
+{
+    [Entity]
+    public interface IReferenceSituationRaw
+    {
+        int Id { get; set; }
+        int? SensorId { get; set; }
+        int? MeasTaskId { get; set; }
+        IMeasTask MEASTASK { get; set; }
+        ISensor SENSOR { get; set; }
+    }
+}
+
