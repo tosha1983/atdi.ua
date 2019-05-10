8d568bb55bf3c583d36f040c15debdf7485015b7
LIS <ProgramSilver@gmail.com>
2019-05-10 10:58:17 +0300

    Check SignalingMeasTask  class

--- Dev/DataModels/Atdi.DataModels.Sdrns.Device/Tasks/Signaling/SignalingMeasTask.cs
+++ Dev/DataModels/Atdi.DataModels.Sdrns.Device/Tasks/Signaling/SignalingMeasTask.cs
@@ -8,12 +8,12 @@ namespace Atdi.DataModels.Sdrns.Device
 {
     public class SignalingMeasTask
     {
-        public bool CompareTraceJustWithRefLevels { get; set; }
-        public bool AutoDivisionEmitting { get; set; }
-        public double DifferenceMaxMax { get; set; }
-        public bool FiltrationTrace { get; set; }
-        public double allowableExcess_dB { get; set; }
-        public int SignalizationNCount { get; set; }
-        public int SignalizationNChenal { get; set; }
+        public bool? CompareTraceJustWithRefLevels { get; set; }
+        public bool? AutoDivisionEmitting { get; set; }
+        public double? DifferenceMaxMax { get; set; }
+        public bool? FiltrationTrace { get; set; }
+        public double? allowableExcess_dB { get; set; }
+        public int? SignalizationNCount { get; set; }
+        public int? SignalizationNChenal { get; set; }
     }
 }
