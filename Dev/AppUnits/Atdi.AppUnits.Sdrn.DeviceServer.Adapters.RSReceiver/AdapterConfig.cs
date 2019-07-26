a05fbd93e9735aeaf44d9b0dd5382a14d5729820
Евгений Ковалевский <jacks.des@gmail.com>
2019-07-26 12:08:04 +0300

    Merge

---
+++ Dev/AppUnits/Atdi.AppUnits.Sdrn.DeviceServer.Adapters.RSReceiver/AdapterConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atdi.AppUnits.Sdrn.DeviceServer.Adapters.RSReceiver
+{
+    public class AdapterConfig
+    {
+        public string IPAddress { get; set; }
+        public int Port { get; set; }
+    }
+}
