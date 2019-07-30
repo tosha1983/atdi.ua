1fc565567915c2474a30f636d03b66963d819408
Anton Kharkhardin <Anton.Kharkhardin@outlook.com>
2019-07-30 16:49:33 +0300

    Add field Frequency

--- Dev/DataModels/Atdi.DataModels.Sdrns.Server.Entities/DataLayer/SqlServer/Tables/ICSC.RES_MEAS_STATION.sql
+++ Dev/DataModels/Atdi.DataModels.Sdrns.Server.Entities/DataLayer/SqlServer/Tables/ICSC.RES_MEAS_STATION.sql
@@ -5,6 +5,7 @@ create table [ICSC].[RES_MEAS_STATION] (
 	[ID] 	[bigint] identity (1,1) not null,
 	[GLOBAL_SID]	[nvarchar](250) null,
 	[MEAS_GLOBAL_SID]	[nvarchar](250) null,
+	[FREQUENCY] [float] not null,
 	[STATUS]	[nvarchar](250) null,
 	[RES_MEAS_ID]	[bigint] not null,
 	[STANDARD]	[nvarchar](50) null,
