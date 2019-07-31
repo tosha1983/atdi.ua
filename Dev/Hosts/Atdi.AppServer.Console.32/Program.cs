4174059a55c078b846b2b631c4e26e4b985057df
Andrey Kovpak <kovpak@outlook.com>
2019-08-01 00:14:49 +0300

    Build Release Version 2.1

---
+++ Dev/Hosts/Atdi.AppServer.Console.32/Program.cs
@@ -0,0 +1,32 @@
+﻿using Atdi.Platform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atdi.AppServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = PlatformConfigurator.BuildHost())
+            {
+                try
+                {
+                    host.Start();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);
+                }
+
+                Console.ReadKey();
+                host.Stop();
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Dev/Hosts/Atdi.AppServer.Svchost.64/HostService.cs
@@ -0,0 +1,106 @@
+﻿using Atdi.Platform;
+using Atdi.Platform.AppServer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atdi.AppServer.Svchost
+{
+    public enum ServiceState
+    {
+        SERVICE_STOPPED = 0x00000001,
+        SERVICE_START_PENDING = 0x00000002,
+        SERVICE_STOP_PENDING = 0x00000003,
+        SERVICE_RUNNING = 0x00000004,
+        SERVICE_CONTINUE_PENDING = 0x00000005,
+        SERVICE_PAUSE_PENDING = 0x00000006,
+        SERVICE_PAUSED = 0x00000007,
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct ServiceStatus
+    {
+        public int dwServiceType;
+        public ServiceState dwCurrentState;
+        public int dwControlsAccepted;
+        public int dwWin32ExitCode;
+        public int dwServiceSpecificExitCode;
+        public int dwCheckPoint;
+        public int dwWaitHint;
+    };
+
+    public partial class HostService : ServiceBase
+    {
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool SetServiceStatus(IntPtr handle, ref ServiceStatus serviceStatus);
+
+        private IServerHost _serverHost;
+
+        public HostService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if (_serverHost != null)
+            {
+                return;
+            }
+
+            var serviceStatus = new ServiceStatus();
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_START_PENDING;
+            serviceStatus.dwWaitHint = 60000;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+
+            try
+            {
+                this._serverHost = PlatformConfigurator.BuildHost();
+                this._serverHost.Start();
+
+                serviceStatus.dwCurrentState = ServiceState.SERVICE_RUNNING;
+                SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+            }
+            catch (Exception)
+            {
+                serviceStatus.dwCurrentState = ServiceState.SERVICE_STOPPED;
+                SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+                throw;
+            }
+            
+        }
+
+        protected override void OnStop()
+        {
+            if (_serverHost == null)
+            {
+                return;
+            }
+
+            var serviceStatus = new ServiceStatus();
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_STOP_PENDING;
+            serviceStatus.dwWaitHint = 60000;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+
+            try
+            {
+                this._serverHost.Stop();
+                this._serverHost.Dispose();
+                this._serverHost = null;
+            }
+            catch(Exception)
+            {
+                serviceStatus.dwCurrentState = ServiceState.SERVICE_RUNNING;
+                SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+                throw;
+            }
+        }
+    }
+}
