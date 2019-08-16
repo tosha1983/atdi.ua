ee2b1e8aaca81fc912dafcc690cb46daa90a6205
Andrey Kovpak <kovpak@outlook.com>
2019-08-16 14:24:02 +0300

    Connected pipeline part to SDRN Server and Masrter Role

--- Dev/AppUnits/Atdi.AppUnits.Sdrn.Server.PrimaryHandlers/SdrnServerPrimaryHandlersComponent.cs
+++ Dev/AppUnits/Atdi.AppUnits.Sdrn.Server.PrimaryHandlers/SdrnServerPrimaryHandlersComponent.cs
@@ -8,7 +8,9 @@ using System.Threading.Tasks;
 using System.Reflection;
 using Atdi.Platform.AppComponent;
 using Atdi.Platform.Logging;
-
+using Atdi.Platform.Workflows;
+using Atdi.DataModels.Sdrns.Server;
+using Atdi.AppUnits.Sdrn.Server.PrimaryHandlers.PipelineHandlers;
 
 namespace Atdi.AppUnits.Sdrn.Server.PrimaryHandlers
 {
@@ -25,10 +27,22 @@ namespace Atdi.AppUnits.Sdrn.Server.PrimaryHandlers
         {
             var exampleConfig = this.Config.Extract<Configs>();
             this.Container.RegisterInstance(exampleConfig, ServiceLifetime.Singleton);
+
+
+            // регистрация  обрабочика конвеера в DI-окружении
+            this.Container.Register<ClientMeasTasksPipelineHandler, ClientMeasTasksPipelineHandler>(ServiceLifetime.Singleton);
         }
 
         protected override void OnActivateUnit()
         {
+            var pipelineSite = this.Resolver.Resolve<IPipelineSite>();
+
+            // декларация конвейера
+            var tasksPipeline = pipelineSite.Declare<ClientMeasTaskPipebox, ClientMeasTaskPiperesult>(Pipelines.ClientMeasTasks);
+            var commandsPipeline = pipelineSite.Declare<ClientMeasTaskPipebox, ClientMeasTaskPiperesult>(Pipelines.ClientCommands);
+            
+            // регистрация обработчика
+            tasksPipeline.Register(typeof(ClientMeasTasksPipelineHandler), PipelineHandlerRegistrationOptions.Last);
         }
 
         protected override void OnDeactivateUnit()
---
+++ Dev/DataModels/Atdi.DataModels.Sdrns.Server/ClientMeasTaskPipebox.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atdi.DataModels.Sdrns.Server
+{
+    public class ClientMeasTaskPipebox
+    {
+    }
+
+    public class ClientMeasTaskPiperesult
+    {
+    }
+}
