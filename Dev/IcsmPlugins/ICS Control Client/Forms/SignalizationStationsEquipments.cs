5a09ca43c58ab470664a57b3dd9f08bcbdbb089e
Anton Kharkhardin <Anton.Kharkhardin@outlook.com>
2019-08-19 15:07:44 +0300

    Complete task #602 - Singnalization Compair with Eqipments Mask

---
+++ Dev/IcsmPlugins/ICS Control Client/Forms/SignalizationStationsEquipments.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using XICSM.ICSControlClient.Models;
+using System.IO;
+using System.Windows.Markup;
+using System.Windows;
+using XICSM.ICSControlClient.ViewModels;
+using SDR = Atdi.Contracts.WcfServices.Sdrn.Server;
+using XICSM.ICSControlClient.Models.Views;
+
+namespace XICSM.ICSControlClient.Forms
+{
+    public partial class SignalizationStationsEquipments : Form
+    {
+        private ElementHost _wpfElementHost;
+        private SDR.MeasurementResults _measResult;
+        private StationsEquipment[] _stationData;
+        private EmittingViewModel[] _emittings;
+        private double[] _selectedRangeX;
+        private Stack<double[]> _zoomHistory = new Stack<double[]>();
+
+        public SignalizationStationsEquipments(StationsEquipment[] stationData, EmittingViewModel[] emittings, SDR.MeasurementResults measResult, Stack<double[]> zoomHistory, double[] selectedRangeX)
+        {
+            this._stationData = stationData;
+            this._emittings = emittings;
+            this._measResult = measResult;
+            this._zoomHistory = zoomHistory;
+            this._selectedRangeX = selectedRangeX;
+            InitializeComponent();
+        }
+
+        private void SignalizationStationsEquipments_Load(object sender, EventArgs e)
+        {
+            _wpfElementHost = new ElementHost();
+            _wpfElementHost.Dock = DockStyle.Fill;
+            this.Controls.Add(_wpfElementHost);
+
+
+            var appFolder = Path.GetDirectoryName(System.Windows.Forms.Application.ExecutablePath);
+
+            var fileName = Path.Combine(appFolder, "XICSM_ICSControlClient\\Xaml\\SignalizationStationsEquipmentsForm.xaml");
+            using (var fileStream = new FileStream(fileName, FileMode.Open))
+            {
+                this._wpfElementHost.Child = (UIElement)XamlReader.Load(fileStream);
+                (this._wpfElementHost.Child as System.Windows.Controls.UserControl).DataContext = new SignalizationStationsEquipmentsViewModel(this._stationData, this._emittings, this._measResult, this._zoomHistory, this._selectedRangeX, this);
+            }
+        }
+    }
+}
---
+++ Dev/IcsmPlugins/ICS Control Client/Models/WcfDataApadters/StationsEquipmentDataAdapter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using M = XICSM.ICSControlClient.Models;
+using VM = XICSM.ICSControlClient.Models.Views;
+using XICSM.ICSControlClient.Environment.Wpf;
+
+namespace XICSM.ICSControlClient.Models.WcfDataApadters
+{
+    public class StationsEquipmentDataAdapter : WpfDataAdapter<M.StationsEquipment, VM.StationsEquipmentViewModel, StationsEquipmentDataAdapter>
+    {
+        protected override Func<M.StationsEquipment, VM.StationsEquipmentViewModel> GetMapper()
+        {
+            return Mappers.Map;
+        }
+    }
+}
