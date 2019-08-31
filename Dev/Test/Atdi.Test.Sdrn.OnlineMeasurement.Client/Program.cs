8632d82e45deffb9f392612b1695264eef73d9a9
LIS <ProgramSilver@gmail.com>
2019-08-31 11:28:28 +0300

    Modify Program.cs

--- Dev/Test/Atdi.Test.Sdrn.OnlineMeasurement.Client/Program.cs
+++ Dev/Test/Atdi.Test.Sdrn.OnlineMeasurement.Client/Program.cs
@@ -58,7 +58,17 @@ namespace Atdi.Test.Sdrn.OnlineMeasurement.Client
                         Container = new ClientMeasTaskData
                         {
                             SensorToken = status.SensorToken,
-                            SomeMeasType = 3
+                            Att_dB = 21,
+                            DetectorType = DetectorType.Auto,
+                            FreqStart_MHz = 935,
+                            FreqStop_MHz = 960,
+                            OnlineMeasType = OnlineMeasType.Level,
+                            PreAmp_dB = 11,
+                            RBW_kHz = 1,
+                            RefLevel_dBm = -1,
+                            SweepTime_s = 0.001,
+                            TraceCount = 10,
+                            TraceType = TraceType.Auto
                         }
                     };
                     var json = JsonConvert.SerializeObject(message);
