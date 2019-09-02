386cf2ffe86cd3641399bb799778f1c56969a2c5
LIS <ProgramSilver@gmail.com>
2019-09-02 17:28:05 +0300

    Add new class DeviceServerReturnFailureReasonAdapter into Atdi.DataModels.Sdrns.Device.OnlineMeasurement

---
+++ Dev/DataModels/Atdi.DataModels.Sdrns.Device/OnlineMeasurement/DeviceServerReturnFailureReasonAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Atdi.DataModels.Sdrns.Device.OnlineMeasurement
+{
+    [Serializable]
+    public class DeviceServerReturnFailureReasonAdapter : DeviceServerResult
+    {
+        /// <summary>
+        /// Поле для передачи контекста ошибки при ее возникновении в адаптере 
+        /// </summary>
+        public string FailureReason { get; set; }
+
+    }
+
+
+
+
+}
