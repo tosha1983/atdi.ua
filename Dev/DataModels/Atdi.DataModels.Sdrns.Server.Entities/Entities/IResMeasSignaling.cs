6a5b725e7e79252fd4b536304bdcb5e42ccae7e8
Anton Kharkhardin <Anton.Kharkhardin@outlook.com>
2019-09-05 12:54:12 +0300

    Add Meas result signalization worker

---
+++ Dev/DataModels/Atdi.DataModels.Sdrns.Server.Entities/Entities/IResMeasSignaling.cs
@@ -0,0 +1,21 @@
+﻿using Atdi.DataModels.EntityOrm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atdi.DataModels.Sdrns.Server.Entities
+{
+    [EntityPrimaryKeyAttribute]
+    public interface IResMeasSignaling_PK
+    {
+        long Id { get; set; }
+    }
+    [Entity]
+    public interface IResMeasSignaling : IResMeasSignaling_PK
+    {
+        bool IsSend { get; set; }
+        IResMeas RES_MEAS { get; set; }
+    }
+}
