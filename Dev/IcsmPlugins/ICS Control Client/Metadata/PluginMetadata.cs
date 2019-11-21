30a02d27eacae3f30a30ebc913ea2f4c3369c876
Anton Kharkhardin <Anton.Kharkhardin@outlook.com>
2019-11-21 16:08:13 +0200

    Change schema version

--- Dev/IcsmPlugins/ICS Control Client/Metadata/PluginMetadata.cs
+++ Dev/IcsmPlugins/ICS Control Client/Metadata/PluginMetadata.cs
@@ -12,7 +12,7 @@ namespace XICSM.ICSControlClient
     {
         public static readonly string Title = "ICS Control Client";
         public static readonly string Ident = "ICSControlClient";
-        public static readonly double SchemaVersion = 20180216.1443;
+        public static readonly double SchemaVersion = 20191121.1607;
 
         public class Processes
         {
