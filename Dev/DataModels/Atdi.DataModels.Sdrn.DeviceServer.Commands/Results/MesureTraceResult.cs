cb36a14dfc46175b3fd61227fedfee1dc9901ac3
LIS <ProgramSilver@gmail.com>
2019-12-09 15:07:33 +0200

    Extended model Atdi.DataModels.Sdrn.DeviceServer.Commands.Results.MesureTraceResult

--- Dev/DataModels/Atdi.DataModels.Sdrn.DeviceServer.Commands/Results/MesureTraceResult.cs
+++ Dev/DataModels/Atdi.DataModels.Sdrn.DeviceServer.Commands/Results/MesureTraceResult.cs
@@ -14,7 +14,9 @@ namespace Atdi.DataModels.Sdrn.DeviceServer.Commands.Results
         {
         }
         public float[] Level;
-        public double[] Freq_Hz;
+        public double FrequencyStart_Hz; //Фактическая начальная частота
+        public double FrequencyStep_Hz; // Реальный шаг в сетке частот
+        public int LevelMaxIndex; // последний значимый элемент в индексе массивов
         public long TimeStamp; //Тики относительно new DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc)
         public int RefLevel_dBm;//Возвращает в любом случаее текущий установленный
         public int Att_dB;//Возвращает в любом случаее текущий установленный
