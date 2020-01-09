a1c2858f25340b02e4b2e8142c8490c7a3b8e35a
Andrey Kovpak <kovpak@outlook.com>
2020-01-09 11:16:34 +0200

    Updated

--- Dev/Modules/Atdi.Modules.LicenseGenerator/Program.cs
+++ Dev/Modules/Atdi.Modules.LicenseGenerator/Program.cs
@@ -32,10 +32,13 @@ namespace Atdi.Modules.LicenseGenerator
 
             //UpdatePeriod_ICSControl_ForUDCR_2020(@"C:\Projects\Licensing\UDCR\Sdrn\Licenses_2019");
 
-            UpdatePeriod_WebQueryAppServer_ForBosny_version_1_2020();
-            UpdatePeriod_WebQueryAppServer_ForBosny_version_2_2020();
+            //UpdatePeriod_WebQueryAppServer_ForBosny_version_1_2020();
+            //UpdatePeriod_WebQueryAppServer_ForBosny_version_2_2020();
 
-            Console.WriteLine("Process was finished");
+            UpdatePeriod_ICSControl_ForTest_2020();
+
+
+			Console.WriteLine("Process was finished");
             Console.ReadKey();
         }
 
@@ -184,7 +187,85 @@ namespace Atdi.Modules.LicenseGenerator
 
         }
 
-        static void UpdatePeriod_ICSControl_ForTest(string path)
+		static void UpdatePeriod_ICSControl_ForTest_2020()
+		{
+			var startDate = new DateTime(2018, 12, 25);
+			var stopDate = new DateTime(2022, 1, 1);
+
+			var ownerId = "OID-BD12-A00-N00";
+			var ownerKey = "BD12-A00";
+			var year = (ushort)2021;
+
+			// WebQuery Web Portal
+			//var outPath = @"C:\Projects\Licensing\UDCR\WebQuery\AppServer\Licenses_2020";
+
+			//UpdateLicesePeriod2(
+			//	sourcefileName: "C:\\Projects\\Licensing\\UDCR\\WebQuery\\AppServer\\BD13-G65\\ServerLicense\\LIC-WQASBD13-G65-605.APPSRV-WQBD13-G65-8673.lic",
+			//	productKey: "IV0O-6R1S-NU0P-6GWS-EWRX",
+			//	outPath: outPath,
+			//	ownerId: ownerId,
+			//	ownerKey: ownerKey,
+			//	startDate: startDate,
+			//	stopDate: stopDate,
+			//	year: year);
+
+			// WebQuery Web Portal
+			//outPath = @"C:\Projects\Licensing\UDCR\WebQuery\WebPortal\Licenses_2020";
+
+			//UpdateLicesePeriod2(
+			//	sourcefileName: "C:\\Projects\\Licensing\\UDCR\\WebQuery\\WebPortal\\BD13-G65\\ServerLicense\\LIC-WQWPBD13-G65-550.WBP-WQBD13-G65-9714.lic",
+			//	productKey: "NP0W-Q6B4-U0W3-9ZW1-IE7R",
+			//	outPath: outPath,
+			//	ownerId: ownerId,
+			//	ownerKey: ownerKey,
+			//	startDate: startDate,
+			//	stopDate: stopDate,
+			//	year: year);
+
+			//// SDRN
+			var outPath = @"C:\Projects\Licensing\Test\Sdrn\Licenses_2020";
+			//// server
+			//UpdateLicesePeriod2(
+			//	sourcefileName: @"C:\Projects\Licensing\UDCR\Sdrn\Licenses_2019\BD13-G65\ServerLicense\LIC-SBD13-G65-607.SDRNSV-SBD13-G65-3690.lic",
+			//	productKey: "MGDM-RD0E-ER0I-6GJR-0DCS",
+			//	outPath: outPath,
+			//	ownerId: ownerId,
+			//	ownerKey: ownerKey,
+			//	startDate: startDate,
+			//	stopDate: stopDate,
+			//	year: year);
+
+			var data = new string[][]
+			{
+				new string[] { "LIC-DBD12-A00-187.SENSOR-DBD12-A00-1280", "0ZB0-DVZR-ATI1-WIHB-NC1B" },
+				new string[] { "LIC-DBD12-A00-192.SENSOR-DBD12-A00-0618", "BCBC-ECA1-L9EC-IR7E-V1AD" },
+				new string[] { "LIC-DBD12-A00-333.SENSOR-DBD12-A00-5140", "CCMR-RBZN-2C1D-OE40-EC23" },
+				new string[] { "LIC-DBD12-A00-442.SENSOR-DBD12-A00-5169", "ADL6-EDCV-NC1I-CE1N-DLH0" },
+				new string[] { "LIC-DBD12-A00-511.SENSOR-DBD12-A00-8429", "020B-CJHO-TDNI-ZEEO-2KDI" },
+				new string[] { "LIC-DBD12-A00-542.SENSOR-DBD12-A00-9792", "N8XE-HLRQ-DERB-NLIV-0DX2" },
+				new string[] { "LIC-DBD12-A00-668.SENSOR-DBD12-A00-8485", "ROL9-XSMC-WDCT-62ST-R5ER" },
+
+				new string[] { "LIC-DBD12-A00-722.SENSOR-DBD12-A00-1692", "BE1D-RLNN-S0S6-EN42-0028" },
+				new string[] { "LIC-DBD12-A00-787.SENSOR-DBD12-A00-3828", "NTC0-IODJ-7SC0-01EB-DVRV" },
+				new string[] { "LIC-DBD12-A00-878.SENSOR-DBD12-A00-8918", "0VE1-OCOL-S4S0-C1D1-SEXB" },
+			};
+
+			foreach (var item in data)
+			{
+				UpdateLicesePeriod2(
+				sourcefileName: @"C:\Projects\Licensing\Test\Sdrn\Licenses_2019\BD12-A00\DeviceLicense\" + item[0] + ".lic",
+				productKey: item[1],
+				outPath: outPath,
+				ownerId: ownerId,
+				ownerKey: ownerKey,
+				startDate: startDate,
+				stopDate: stopDate,
+				year: year);
+			}
+			// dev 1
+
+		}
+		static void UpdatePeriod_ICSControl_ForTest(string path)
         {
             var startDate = new DateTime(2018, 12, 25);
             var stopDate = new DateTime(2020, 1, 1);
