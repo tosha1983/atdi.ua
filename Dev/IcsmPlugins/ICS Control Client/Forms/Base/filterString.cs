72b44b8a9db352825ead9ce92827e57c7d5bbe0b
Anton Kharkhardin <Anton.Kharkhardin@outlook.com>
2020-03-23 20:57:19 +0200

    ReBuild Plugin

---
+++ Dev/IcsmPlugins/ICS Control Client/Forms/Base/filterString.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ICSM;
+
+namespace XICSM.ICSControlClient.Forms
+{
+    public partial class filterString : Form
+    {
+        string _filterValue = "";
+
+        public bool IsPresOK = false;
+
+        public filterString()
+        {
+            InitializeComponent();
+        }
+        public string FilterValue
+        {
+            get => this._filterValue;
+            set
+            {
+                this._filterValue = value;
+                txtFilter.Text = value;
+            }
+        }
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            IsPresOK = true;
+            this.Close();
+        }
+        private void ValueChanged(object sender, EventArgs e)
+        {
+            FilterValue = txtFilter.Text;
+        }
+    }
+}
