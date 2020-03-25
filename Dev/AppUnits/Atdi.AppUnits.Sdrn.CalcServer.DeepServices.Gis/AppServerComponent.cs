afb8876c26889e02f01e23dd112ae0cbeb3a79c3
Andrey Kovpak <kovpak@outlook.com>
2020-03-25 14:38:31 +0200

    Added GIS Deep Service

---
+++ Dev/AppUnits/Atdi.AppUnits.Sdrn.CalcServer.DeepServices.Gis/AppServerComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Atdi.AppUnits.Sdrn.CalcServer.DeepServices.Gis
+{
+    public class AppServerComponent : AppUnitComponent
+	{
+		public AppServerComponent()
+			: base("SdrnCalcServerDeepServicesGisAppUnit")
+		{
+
+		}
+
+		protected override void OnInstallUnit()
+		{
+			
+		}
+	}
+}
---
+++ Dev/DataModels/Atdi.DataModels.Sdrn.CalcServer.Entities/Entities/IPointFieldStrengthCalcTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Atdi.DataModels.EntityOrm;
+
+namespace Atdi.DataModels.Sdrn.CalcServer.Entities
+{
+	[EntityPrimaryKey]
+	public interface IPointFieldStrengthCalcTask_PK : ICalcTask_PK
+	{
+	}
+
+	[Entity]
+	public interface IPointFieldStrengthCalcTask : ICalcTask, IPointFieldStrengthCalcTask_PK
+	{
+		IContextStation STATION { get; set; }
+
+		double PointLongitude_DEC { get; set; }
+
+		double PointLatitude_DEC { get; set; }
+
+		double PointAltitude_m { get; set; }
+	}
+	
+}
