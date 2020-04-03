cefabd59bb8b0d2d9c0767fac7fa1321bcf131ad
maxkolomytsev <maxkolomytsev@gmail.com>
2020-04-03 19:17:17 +0300

    Add ITU 525

---
+++ Dev/AppUnits/Atdi.AppUnits.Sdrn.DeepServices.RadioSystem/PropagationCalc/ITU525.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atdi.AppUnits.Sdrn.DeepServices.RadioSystem.Signal
+{
+    internal static class ITU525
+    {
+        public static float Calc(float ha_m, float hb_m, double Freq_MHz, double d_km)
+        {
+            var d = Math.Sqrt((hb_m - ha_m) * (hb_m - ha_m)*0.000001 + d_km * d_km);
+            var Lbf_dB = 32.4 + 20 * Math.Log10(Freq_MHz) + 20 * Math.Log10(d);
+            return (float)Lbf_dB;
+        }
+    }
+}
---
+++ Dev/AppUnits/Atdi.AppUnits.Sdrn.DeepServices.RadioSystem/PropagationCalc/PropagationLoss.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Atdi.DataModels.Sdrn.DeepServices.RadioSystem.PropagationModels;
+using Atdi.DataModels.Sdrn.DeepServices.RadioSystem.SignalService;
+
+namespace Atdi.AppUnits.Sdrn.DeepServices.RadioSystem.Signal
+{
+	internal static class PropagationLoss
+	{
+
+        public static int Calc(CalcLossArgs args)
+        {
+            float Lbf_dB = CalclMainBlock(in args.Model.MainBlock);
+            float Ld_dB = 0;
+            if (args.Model.DiffractionBlock.Available) { Ld_dB = CalcDiffraction(in args.Model.DiffractionBlock);}
+
+            return 0;
+        }
+        private static float CalclMainBlock(in MainCalcBlock args)
+		{
+            float ha_m = 15; // Надо сюда передавать
+            float hb_m = 20; // Надо сюда передавать
+            double Freq_MHz = 1800; // Надо сюда передавать
+            double d_km = 15; // Надо сюда передавать
+            float Lbf_dB = 0;
+            switch (args.ModelType)
+            {
+                case MainCalcBlockModelType.ITU525:
+                case MainCalcBlockModelType.Unknown:
+                default:
+                    Lbf_dB = ITU525.Calc(ha_m, hb_m, Freq_MHz, d_km);
+                    break;
+            }
+            return Lbf_dB;
+		}
+        private static float CalcDiffraction(in DiffractionCalcBlock args)
+        {
+            float Ld_dB = 0;
+            switch (args.ModelType)
+            {
+                case DiffractionCalcBlockModelType.Deygout91:
+                    // вызов модели дегу
+                    break;
+                case DiffractionCalcBlockModelType.Deygout66:
+                case DiffractionCalcBlockModelType.Bullington:
+                case DiffractionCalcBlockModelType.ITU526_15:
+                default:
+                    Ld_dB = 0;
+                    break;
+            }
+            return Ld_dB;
+
+        }
+	}
+}
