664b580db889458942ea0655e6700295eb862e00
Jerzy Syniepolski <jws1412@gmail.com>
2020-04-09 21:57:36 +0300

    Diffraction  and tilt calculation code transfer

--- Dev/AppUnits/Atdi.AppUnits.Sdrn.DeepServices.RadioSystem/PropagationCalc/Deygout91.cs
+++ Dev/AppUnits/Atdi.AppUnits.Sdrn.DeepServices.RadioSystem/PropagationCalc/Deygout91.cs
@@ -8,9 +8,95 @@ namespace Atdi.AppUnits.Sdrn.DeepServices.RadioSystem.Signal
 {
     internal static class Deygout91
     {
-        public static double Calc(double ha_m, double hb_m, double Freq_MHz, double d_km, in short[] profile_m, int profileStart, int profileCount, double rE_km, double subDiffractionLoss)
+        private struct NuMaxOut
         {
-            return 0;
+            public double nuMax;
+            public int nMax;
+        }
+
+        private static NuMaxOut FindMaxNu(double HA, double HB, double Wavelength, double DAB, in short[] Profile, int ProfileStart, int profileCount, double RE)
+        {
+            int profileArrayLength = (profileCount - ProfileStart);
+            double dN = DAB / profileArrayLength;
+            double dAN = dN;
+            double dNB = dN * (profileArrayLength - 1);
+            double inv2rE = 1 / (2 * RE);
+            double invDaB = 1 / DAB;
+
+            double hNMax = Profile[ProfileStart] + dAN * dNB * inv2rE - (HA * dNB + HB * dAN) * invDaB;
+
+            
+            NuMaxOut nuMaxOut = new NuMaxOut
+            {
+                nuMax = (double)(hNMax * Math.Sqrt(2 * DAB / (Wavelength * dAN * dNB))),
+                nMax = ProfileStart
+            };
+            
+            if (profileArrayLength > 1)
+            {
+                for (int n = ProfileStart + 1; n < profileCount; n++)
+                {
+                    dAN = dN * (n - ProfileStart);
+                    dNB = dN * (profileCount - n);
+                    double h = Profile[n] + dAN * dNB * inv2rE - (HA * dNB + HB * dAN) * invDaB;
+
+                    double nuN = (double)(h * Math.Sqrt(2 * DAB / (Wavelength * dAN * dNB)));
+
+                    if (nuN > nuMaxOut.nuMax)
+                    {
+                        nuMaxOut.nuMax = nuN;
+                        nuMaxOut.nMax = n;
+                    }
+                }
+            }
+            return nuMaxOut;
+        }
+
+        private static double J(double Nu)
+        {
+            if (Nu <= -0.78)
+            {
+                return 0.0f;
+            }
+            else
+            {
+                return (double)(6.9 + 20 * Math.Log10(Math.Sqrt(Math.Pow(Nu - 0.1, 2) + 1) + Nu - 0.1));
+            }
+        }
+        
+
+        public static double Calc(double ha_m, double hb_m, double Freq_MHz, double d_km, in short[] profile_m, int profileStart, int profileCount, double rE_km, double SubDiffractionLoss)
+        {
+
+            double dAB = d_km * 1000;
+            double rE = rE_km * 1000;
+            double wavelength = 300 / Freq_MHz;
+            ha_m += profile_m[profileStart];
+            hb_m += profile_m[profileCount];
+
+            NuMaxOut nu;
+            nu = FindMaxNu(ha_m, hb_m, wavelength, dAB, profile_m, profileStart, profileCount, rE);
+
+            double nuP = nu.nuMax;
+
+            double dN = dAB / (profileCount - profileStart);
+            double dAP = dN * nu.nMax;
+
+            double dPB = dN * (profileCount - profileStart - nu.nMax);
+
+            double diffractionLoss_dB = 0;
+
+            if (nuP > -0.78)
+            {
+                NuMaxOut nuT = FindMaxNu(ha_m, profile_m[nu.nMax], wavelength, dAP, in profile_m, profileStart, nu.nMax, rE);
+
+                NuMaxOut nuR = FindMaxNu(profile_m[nu.nMax], hb_m, wavelength, dPB, in profile_m, nu.nMax, profileCount, rE);
+                double C = 10.0f + 0.04 * d_km;
+                diffractionLoss_dB = J(nuP) + (1.0 - Math.Exp(-J(nuP) / 6.0f)) * (J(nuT.nuMax) + J(nuR.nuMax) + C + SubDiffractionLoss);
+            }
+            
+
+            return diffractionLoss_dB;
         }
     }
 
--- Dev/AppUnits/Atdi.AppUnits.Sdrn.DeepServices.RadioSystem/PropagationCalc/ProfilesCalculation.cs
+++ Dev/AppUnits/Atdi.AppUnits.Sdrn.DeepServices.RadioSystem/PropagationCalc/ProfilesCalculation.cs
@@ -10,15 +10,80 @@ namespace Atdi.AppUnits.Sdrn.DeepServices.RadioSystem.Signal
 {
     internal static class ProfilesCalculation
     {
-        public static void CalcTilts(double re_km, double ha_m, double hb_m, double d_km, in short[] profile, int StartPosition, int IndexerCount, out double tiltA_deg, out double tiltB_deg)
+        private static double CalcElevAngleArgs(double H1, double H2, double D, double RE)
         {
-            tiltA_deg = 0;
-            tiltB_deg = 0;
+            if (D > 100)
+            {
+                return (Math.PI / 2 - Math.Acos((Math.Pow(RE + H1, 2) + Math.Pow(D, 2) - Math.Pow(RE + H2, 2)) / (2 * (RE + H1) * D)));
+            }
+            else
+            {
+                return (Math.Atan((H1 - H2) / D) * 180 / Math.PI);
+            }
+            
         }
+
+        public static void CalcTilts(double re_km, double ha_m, double hb_m, double d_km, in short[] Profile, int StartPosition, int IndexerCount, out double tiltA_deg, out double tiltB_deg)
+        {
+           //both implementations requires testing 
+            double dN = d_km * 1000 / (IndexerCount - StartPosition);
+            double h1_m = ha_m + Profile[StartPosition];
+            double h2_m = hb_m + Profile[IndexerCount];
+            double angleArgAB = CalcElevAngleArgs(h1_m, hb_m, d_km * 1000, re_km * 1000);
+            double angleArgBA = CalcElevAngleArgs(h2_m, hb_m, d_km * 1000, re_km * 1000);
+
+            double minAngleArgAB = 1;
+            double minAngleArgBA = 1;
+
+
+            for (int n = StartPosition; n < IndexerCount - 1; n++)
+            {
+                double dAN = dN * (n - StartPosition);
+                double dNB = dN * (IndexerCount - n);
+                
+                double nAngleArgAB = CalcElevAngleArgs(h1_m, Profile[n + StartPosition], dAN, re_km * 1000);
+                double nAngleArgBA = CalcElevAngleArgs(h2_m, Profile[IndexerCount - n], dNB, re_km * 1000);
+
+                if (minAngleArgAB > nAngleArgAB)
+                {
+                    minAngleArgAB = nAngleArgAB;
+                }
+                if (minAngleArgBA > nAngleArgBA)
+                {
+                    minAngleArgBA = nAngleArgBA;
+                }
+            }
+
+
+            if (d_km > 0.1)
+            {
+                tiltA_deg = (Math.PI / 2 - Math.Acos(angleArgAB)) * 180 / Math.PI;
+                tiltB_deg = (Math.PI / 2 - Math.Acos(angleArgBA)) * 180 / Math.PI;
+            }
+            else
+            {
+                tiltA_deg = Math.Atan(angleArgAB) * 180 / Math.PI;
+                tiltB_deg = Math.Atan(angleArgAB) * 180 / Math.PI;
+            }
+            
+        }
+
+
         public static void CalcTilts(double re_km, double h1_m, double h2_m, double d_km, out double tiltA_deg, out double tiltB_deg)
         {
-            tiltA_deg = 0;
-            tiltB_deg = 0;
+            double angleArgAB = CalcElevAngleArgs(h1_m, h2_m, d_km * 1000, re_km * 1000);
+            double angleArgBA = CalcElevAngleArgs(h2_m, h1_m, d_km * 1000, re_km * 1000);
+
+            if (d_km > 0.1)
+            {
+                tiltA_deg = (Math.PI / 2 - Math.Acos(angleArgAB)) * 180 / Math.PI;
+                tiltB_deg = (Math.PI / 2 - Math.Acos(angleArgBA)) * 180 / Math.PI;
+            }
+            else
+            {
+                tiltA_deg = Math.Atan(angleArgAB) * 180 / Math.PI;
+                tiltB_deg = Math.Atan(angleArgAB) * 180 / Math.PI;
+            }
         }
         public static EstimationClutterObstaclesResult[] EstimationClutterObstacles(in CalcLossArgs args)
         {
