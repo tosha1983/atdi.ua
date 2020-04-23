fb946644adb62e29e26524e672a8f25c815804e0
Andrey Kovpak <kovpak@outlook.com>
2020-04-23 15:14:58 +0300

    Added Clutters Description models

---
+++ Dev/DataModels/Atdi.DataModels.Sdrn.Infocenter.Entities/Entities/ICluttersDesc.cs
@@ -0,0 +1,27 @@
+﻿using Atdi.DataModels.EntityOrm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atdi.DataModels.Sdrn.Infocenter.Entities
+{
+    [EntityPrimaryKey]
+    public interface ICluttersDesc_PK
+	{
+        long Id { get; set; }
+    }
+
+    [Entity]
+    public interface ICluttersDesc : ICluttersDesc_PK
+	{
+		IMap MAP { get; set; }
+
+        DateTimeOffset CreatedDate { get; set; }
+
+        string Name { get; set; }
+
+        string Note { get; set; }
+	}
+}
