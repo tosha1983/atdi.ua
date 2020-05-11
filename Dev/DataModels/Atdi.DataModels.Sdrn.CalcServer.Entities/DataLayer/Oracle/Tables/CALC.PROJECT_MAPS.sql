790e8e252704b4da6ff55079a3d433003aa017e6
Grigoriy Serebryakov <sdrnsserver@gmail.com>
2020-05-11 22:10:51 +0300

    Check sql scripts

---
+++ Dev/DataModels/Atdi.DataModels.Sdrn.CalcServer.Entities/DataLayer/Oracle/Tables/CALC.PROJECT_MAPS.sql
@@ -0,0 +1,82 @@
+﻿CREATE TABLE CALC.PROJECT_MAPS
+(
+  ID                   NUMBER(15)               NOT NULL,
+  PROJECT_ID           NUMBER(15)               NOT NULL,
+  MAP_NAME             NVARCHAR2(250)           NOT NULL,
+  MAP_NOTE             NCLOB,
+  OWNER_INSTANCE       NVARCHAR2(250)           NOT NULL,
+  OWNER_MAP_ID         VARCHAR2(36 BYTE)        NOT NULL,
+  CREATED_DATE         TIMESTAMP(7) WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP AT TIME ZONE 'GMT' NOT NULL,
+  STATUS_CODE          NUMBER(3)                NOT NULL,
+  STATUS_NAME          NVARCHAR2(50)            NOT NULL,
+  STATUS_NOTE          NCLOB,
+  STEP_UNIT            NVARCHAR2(10)            NOT NULL,
+  OWNER_AXIS_X_NUMBER  NUMBER(9)                NOT NULL,
+  OWNER_AXIS_X_STEP    NUMBER(9)                NOT NULL,
+  OWNER_AXIS_Y_NUMBER  NUMBER(9)                NOT NULL,
+  OWNER_AXIS_Y_STEP    NUMBER(9)                NOT NULL,
+  OWNER_CRD_UPL_X      NUMBER(9)                NOT NULL,
+  OWNER_CRD_UPL_Y      NUMBER(9)                NOT NULL,
+  AXIS_X_NUMBER        NUMBER(9)                NOT NULL,
+  AXIS_X_STEP          NUMBER(9)                NOT NULL,
+  AXIS_Y_NUMBER        NUMBER(9)                NOT NULL,
+  AXIS_Y_STEP          NUMBER(9)                NOT NULL,
+  CRD_UPL_X            NUMBER(9)                NOT NULL,
+  CRD_UPL_Y            NUMBER(9)                NOT NULL,
+  CRD_LWR_X            NUMBER(9)                NOT NULL,
+  CRD_LWR_Y            NUMBER(9)                NOT NULL
+)
+TABLESPACE USERS
+PCTUSED    0
+PCTFREE    10
+INITRANS   1
+MAXTRANS   255
+STORAGE    (
+            INITIAL          64K
+            NEXT             1M
+            MINEXTENTS       1
+            MAXEXTENTS       UNLIMITED
+            PCTINCREASE      0
+            BUFFER_POOL      DEFAULT
+           )
+LOGGING 
+NOCOMPRESS 
+NOCACHE
+NOPARALLEL
+MONITORING;
+
+
+CREATE UNIQUE INDEX CALC.PROJECT_MAPS_PK ON CALC.PROJECT_MAPS
+(ID)
+LOGGING
+TABLESPACE USERS
+PCTFREE    10
+INITRANS   2
+MAXTRANS   255
+STORAGE    (
+            INITIAL          64K
+            NEXT             1M
+            MINEXTENTS       1
+            MAXEXTENTS       UNLIMITED
+            PCTINCREASE      0
+            BUFFER_POOL      DEFAULT
+           )
+NOPARALLEL;
+
+
+ALTER TABLE CALC.PROJECT_MAPS ADD (
+  CONSTRAINT PROJECT_MAPS_PK
+ PRIMARY KEY
+ (ID)
+    USING INDEX 
+    TABLESPACE USERS
+    PCTFREE    10
+    INITRANS   2
+    MAXTRANS   255
+    STORAGE    (
+                INITIAL          64K
+                NEXT             1M
+                MINEXTENTS       1
+                MAXEXTENTS       UNLIMITED
+                PCTINCREASE      0
+               ));
