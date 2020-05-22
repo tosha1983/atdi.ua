6625e72de3e0878cc1b71001a7f44d984df35c88
Andrey Kovpak <kovpak@outlook.com>
2020-05-22 12:12:05 +0300

    Fixed bugs

--- Dev/AppUnits/Atdi.AppUnits.Sdrn.Infocenter.Integration/AppServerComponent.cs
+++ Dev/AppUnits/Atdi.AppUnits.Sdrn.Infocenter.Integration/AppServerComponent.cs
@@ -29,91 +29,91 @@ namespace Atdi.AppUnits.Sdrn.Infocenter.Integration
 
 			
 
-			this.Container.Register<SdrnAutoImportJob>(ServiceLifetime.Singleton);
+			//this.Container.Register<SdrnAutoImportJob>(ServiceLifetime.Singleton);
 		}
 
 		protected override void OnActivateUnit()
 		{
-			var typeResolver = this.Resolver.Resolve<ITypeResolver>();
-			var appConfig = this.Resolver.Resolve<AppServerComponentConfig>();
+			//var typeResolver = this.Resolver.Resolve<ITypeResolver>();
+			//var appConfig = this.Resolver.Resolve<AppServerComponentConfig>();
 
-			// подключаем обработчики событий
-			var eventDispatcher = this.Resolver.Resolve<IEventDispatcher>();
-			var eventSubscribeInterfaceType = typeof(IEventSubscriber<>);
+			//// подключаем обработчики событий
+			//var eventDispatcher = this.Resolver.Resolve<IEventDispatcher>();
+			//var eventSubscribeInterfaceType = typeof(IEventSubscriber<>);
 
-			var subscriberTypes = typeResolver.ForeachInAllAssemblies(
-					(type) =>
-					{
-						if (!type.IsClass
-						|| type.IsNotPublic
-						|| type.IsAbstract
-						|| type.IsInterface
-						|| type.IsEnum)
-						{
-							return false;
-						}
+			//var subscriberTypes = typeResolver.ForeachInAllAssemblies(
+			//		(type) =>
+			//		{
+			//			if (!type.IsClass
+			//			|| type.IsNotPublic
+			//			|| type.IsAbstract
+			//			|| type.IsInterface
+			//			|| type.IsEnum)
+			//			{
+			//				return false;
+			//			}
 
-						var ft = type.GetInterface(eventSubscribeInterfaceType.FullName);
-						return ft != null;
-					}
-				).ToArray();
+			//			var ft = type.GetInterface(eventSubscribeInterfaceType.FullName);
+			//			return ft != null;
+			//		}
+			//	).ToArray();
 
-			foreach (var subscriberType in subscriberTypes)
-			{
-				try
-				{
-					this.Container.Register(subscriberType, subscriberType, ServiceLifetime.PerThread);
-					Logger.Verbouse(Contexts.ThisComponent, Categories.Registration, Events.HandlerTypeWasRegistered.With(subscriberType.AssemblyQualifiedName));
-				}
-				catch (Exception e)
-				{
-					Logger.Exception(Contexts.ThisComponent, Categories.Registration, e);
-				}
-			}
+			//foreach (var subscriberType in subscriberTypes)
+			//{
+			//	try
+			//	{
+			//		this.Container.Register(subscriberType, subscriberType, ServiceLifetime.PerThread);
+			//		Logger.Verbouse(Contexts.ThisComponent, Categories.Registration, Events.HandlerTypeWasRegistered.With(subscriberType.AssemblyQualifiedName));
+			//	}
+			//	catch (Exception e)
+			//	{
+			//		Logger.Exception(Contexts.ThisComponent, Categories.Registration, e);
+			//	}
+			//}
 
 
-			var hostLoader = this.Resolver.Resolve<IServerHostLoader>();
+			//var hostLoader = this.Resolver.Resolve<IServerHostLoader>();
 
-			hostLoader.RegisterTrigger("Running Event System Subscribers", () =>
-			{
-				foreach (var subscriberType in subscriberTypes)
-				{
-					try
-					{
-						eventDispatcher.Subscribe(subscriberType);
-						Logger.Verbouse(Contexts.ThisComponent, Categories.Subscribing, Events.HandlerTypeWasConnected.With(subscriberType.AssemblyQualifiedName));
-					}
-					catch (Exception e)
-					{
-						Logger.Exception(Contexts.ThisComponent, Categories.Registration, e);
-					}
-				}
-			});
+			//hostLoader.RegisterTrigger("Running Event System Subscribers", () =>
+			//{
+			//	foreach (var subscriberType in subscriberTypes)
+			//	{
+			//		try
+			//		{
+			//			eventDispatcher.Subscribe(subscriberType);
+			//			Logger.Verbouse(Contexts.ThisComponent, Categories.Subscribing, Events.HandlerTypeWasConnected.With(subscriberType.AssemblyQualifiedName));
+			//		}
+			//		catch (Exception e)
+			//		{
+			//			Logger.Exception(Contexts.ThisComponent, Categories.Registration, e);
+			//		}
+			//	}
+			//});
 
-			var jobBroker = this.Resolver.Resolve<IJobBroker>();
-			hostLoader.RegisterTrigger("Running Jobs ...", () =>
-			{
-				var startDelaySeconds = appConfig.AutoImportMapsStartDelay;
-				if (!startDelaySeconds.HasValue)
-				{
-					startDelaySeconds = 0;
-				}
-				var repeatDelaySeconds = appConfig.AutoImportMapsRepeatDelay;
-				if (!repeatDelaySeconds.HasValue)
-				{
-					repeatDelaySeconds = 0;
-				}
-				var jobDef = new JobDefinition<MapsAutoImportJob>()
-				{
-					Name = "Maps Auto Import Job",
-					Recoverable = true,
-					Repeatable = true,
-					StartDelay = new TimeSpan(TimeSpan.TicksPerSecond * startDelaySeconds.Value),
-					RepeatDelay = new TimeSpan(TimeSpan.TicksPerSecond * repeatDelaySeconds.Value)
-				};
+			//var jobBroker = this.Resolver.Resolve<IJobBroker>();
+			//hostLoader.RegisterTrigger("Running Jobs ...", () =>
+			//{
+			//	var startDelaySeconds = appConfig.AutoImportMapsStartDelay;
+			//	if (!startDelaySeconds.HasValue)
+			//	{
+			//		startDelaySeconds = 0;
+			//	}
+			//	var repeatDelaySeconds = appConfig.AutoImportMapsRepeatDelay;
+			//	if (!repeatDelaySeconds.HasValue)
+			//	{
+			//		repeatDelaySeconds = 0;
+			//	}
+			//	var jobDef = new JobDefinition<MapsAutoImportJob>()
+			//	{
+			//		Name = "Maps Auto Import Job",
+			//		Recoverable = true,
+			//		Repeatable = true,
+			//		StartDelay = new TimeSpan(TimeSpan.TicksPerSecond * startDelaySeconds.Value),
+			//		RepeatDelay = new TimeSpan(TimeSpan.TicksPerSecond * repeatDelaySeconds.Value)
+			//	};
 
-				jobBroker.Run(jobDef);
-			});
+			//	jobBroker.Run(jobDef);
+			//});
 		}
 	}
 }
