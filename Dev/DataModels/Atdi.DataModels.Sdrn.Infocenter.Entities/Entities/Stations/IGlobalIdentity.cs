339369091b15257497958054c7d64f15e6cf7697
Andrey Kovpak <kovpak@outlook.com>
2020-05-25 15:26:21 +0300

    Added new model to SDRN Infocenter

---
+++ Dev/DataModels/Atdi.DataModels.Sdrn.Infocenter.Entities/Entities/Stations/IGlobalIdentity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atdi.DataModels.Sdrn.Infocenter.Entities.Entities.Stations
+{
+	public interface IGlobalIdentity_PK
+	{
+		long Id { get; set; }
+	}
+	public interface IGlobalIdentity
+	{
+		DateTimeOffset CreatedDate { get; set; }
+
+		string LicenseGsid { get; set; }
+
+		string RealGsid { get; set; }
+
+		string RegionCode { get; set; }
+	}
+}
