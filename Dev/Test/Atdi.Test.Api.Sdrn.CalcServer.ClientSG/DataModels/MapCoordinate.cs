422a712f90b6c4d86337bc081f48121ae091bc89
Grigoriy Serebryakov <sdrnsserver@gmail.com>
2020-05-25 11:37:38 +0300

    add test project for #RFC 711

---
+++ Dev/Test/Atdi.Test.Api.Sdrn.CalcServer.ClientSG/DataModels/MapCoordinate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atdi.Test.Api.Sdrn.CalcServer.Client.DataModels
+{
+	internal struct MapCoordinate
+	{
+		public int X;
+		public int Y;
+
+		public override string ToString()
+		{
+			return $"({X},{Y})";
+		}
+	}
+}
