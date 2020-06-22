7cd28fd929092ce1187c3d9c678bcca0eb877137
Grigoriy Serebryakov <sdrnsserver@gmail.com>
2020-06-22 09:17:56 +0300

    Add sql scripts for CALCTASKS_GN06_ARGS and CALCTASKS_GN06_ARGS_DEF tables

--- Dev/DataModels/Atdi.DataModels.Sdrn.CalcServer.Entities/Entities/Tasks/IGn06ArgsBase.cs
+++ Dev/DataModels/Atdi.DataModels.Sdrn.CalcServer.Entities/Entities/Tasks/IGn06ArgsBase.cs
@@ -5,13 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 using Atdi.DataModels.EntityOrm;
 
+
 namespace Atdi.DataModels.Sdrn.CalcServer.Entities.Tasks
 {
 
 	[Entity]
 	public interface IGn06ArgsBase
 	{
-		
-	}
+        float AzimuthStep_deg { get; set; }
+        bool AdditionalContoursByDistances { get; set; }
+        int[] Distances { get; set; }
+        bool ContureByFieldStrength { get; set; }
+        int[] FieldStrength { get; set; }
+        int SubscribersHeight { get; set; }
+        double PercentageTime { get; set; }
+        bool UseEffectiveHeight { get; set; }
+        string CalculationType { get; set; }
+        string SourceBroadcasting { get; set; }
+        byte[] BroadcastingAssignments { get; set; }
+        byte[] BroadcastingAllotments { get; set; }
+    }
 	
 }
