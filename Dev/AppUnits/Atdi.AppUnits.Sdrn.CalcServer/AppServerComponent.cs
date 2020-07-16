a9421541ac1b8ae937720835a3b0904e805dc19a
Andrey Kovpak <kovpak@outlook.com>
2020-07-16 15:11:05 +0300

    Exclude Events System from Calc Server

--- Dev/AppUnits/Atdi.AppUnits.Sdrn.CalcServer/AppServerComponent.cs
+++ Dev/AppUnits/Atdi.AppUnits.Sdrn.CalcServer/AppServerComponent.cs
@@ -42,27 +42,27 @@ namespace Atdi.AppUnits.Sdrn.CalcServer
 			this.Container.Register<ITaskDispatcher, TaskDispatcher>(ServiceLifetime.Singleton);
 
 			// система событий
-			var eventSiteConfig = new EventSiteConfig();
-			eventSiteConfig.SetValue(EventSiteConfig.ApiVersion, componentConfig.EventSystemApiVersion);
-			eventSiteConfig.SetValue(EventSiteConfig.AppName, componentConfig.EventSystemAppName);
-			eventSiteConfig.SetValue(EventSiteConfig.ErrorsQueueName, componentConfig.EventSystemErrorsQueueName);
-			eventSiteConfig.SetValue(EventSiteConfig.LogQueueName, componentConfig.EventSystemLogQueueName);
-			eventSiteConfig.SetValue(EventSiteConfig.EventBusHost, componentConfig.EventSystemEventBusHost);
-			eventSiteConfig.SetValue(EventSiteConfig.EventBusPassword, componentConfig.EventSystemEventBusPassword);
-			eventSiteConfig.SetValue(EventSiteConfig.EventBusPort, componentConfig.EventSystemEventBusPort);
-			eventSiteConfig.SetValue(EventSiteConfig.EventBusUser, componentConfig.EventSystemEventBusUser);
-			eventSiteConfig.SetValue(EventSiteConfig.EventBusVirtualHost, componentConfig.EventSystemEventBusVirtualHost);
-			eventSiteConfig.SetValue(EventSiteConfig.EventExchange, componentConfig.EventSystemEventExchange);
-			eventSiteConfig.SetValue(EventSiteConfig.EventQueueNamePart, componentConfig.EventSystemEventQueueNamePart);
-			eventSiteConfig.SetValue(EventSiteConfig.UseCompression, componentConfig.EventSystemUseCompression);
-			eventSiteConfig.SetValue(EventSiteConfig.UseEncryption, componentConfig.EventSystemUseEncryption);
-			this.Container.RegisterInstance<IEventSiteConfig>(eventSiteConfig, ServiceLifetime.Singleton);
+			//var eventSiteConfig = new EventSiteConfig();
+			//eventSiteConfig.SetValue(EventSiteConfig.ApiVersion, componentConfig.EventSystemApiVersion);
+			//eventSiteConfig.SetValue(EventSiteConfig.AppName, componentConfig.EventSystemAppName);
+			//eventSiteConfig.SetValue(EventSiteConfig.ErrorsQueueName, componentConfig.EventSystemErrorsQueueName);
+			//eventSiteConfig.SetValue(EventSiteConfig.LogQueueName, componentConfig.EventSystemLogQueueName);
+			//eventSiteConfig.SetValue(EventSiteConfig.EventBusHost, componentConfig.EventSystemEventBusHost);
+			//eventSiteConfig.SetValue(EventSiteConfig.EventBusPassword, componentConfig.EventSystemEventBusPassword);
+			//eventSiteConfig.SetValue(EventSiteConfig.EventBusPort, componentConfig.EventSystemEventBusPort);
+			//eventSiteConfig.SetValue(EventSiteConfig.EventBusUser, componentConfig.EventSystemEventBusUser);
+			//eventSiteConfig.SetValue(EventSiteConfig.EventBusVirtualHost, componentConfig.EventSystemEventBusVirtualHost);
+			//eventSiteConfig.SetValue(EventSiteConfig.EventExchange, componentConfig.EventSystemEventExchange);
+			//eventSiteConfig.SetValue(EventSiteConfig.EventQueueNamePart, componentConfig.EventSystemEventQueueNamePart);
+			//eventSiteConfig.SetValue(EventSiteConfig.UseCompression, componentConfig.EventSystemUseCompression);
+			//eventSiteConfig.SetValue(EventSiteConfig.UseEncryption, componentConfig.EventSystemUseEncryption);
+			//this.Container.RegisterInstance<IEventSiteConfig>(eventSiteConfig, ServiceLifetime.Singleton);
 
-			this.Container.Register<IEventSystemObserver, EventSystemObserver>(ServiceLifetime.Singleton);
-			this.Container.Register<ISubscriberActivator, EventSystemSubscriberActivator>(ServiceLifetime.Singleton);
-			this.Container.Register<IEventSite, EventSite>(ServiceLifetime.Singleton);
-			this.Container.Register<IEventEmitter, EventEmitter>(ServiceLifetime.PerThread);
-			this.Container.Register<IEventDispatcher, EventDispatcher>(ServiceLifetime.Singleton);
+			//this.Container.Register<IEventSystemObserver, EventSystemObserver>(ServiceLifetime.Singleton);
+			//this.Container.Register<ISubscriberActivator, EventSystemSubscriberActivator>(ServiceLifetime.Singleton);
+			//this.Container.Register<IEventSite, EventSite>(ServiceLifetime.Singleton);
+			//this.Container.Register<IEventEmitter, EventEmitter>(ServiceLifetime.PerThread);
+			//this.Container.Register<IEventDispatcher, EventDispatcher>(ServiceLifetime.Singleton);
 
 			// шина данных 
 			
@@ -165,57 +165,58 @@ namespace Atdi.AppUnits.Sdrn.CalcServer
 			
 
 			// подключаем обработчики событий
-			var eventDispatcher = this.Resolver.Resolve<IEventDispatcher>();
-			var eventSubscribeInterfaceType = typeof(IEventSubscriber<>);
+			// пока отключаем систему событий
+			//var eventDispatcher = this.Resolver.Resolve<IEventDispatcher>();
+			//var eventSubscribeInterfaceType = typeof(IEventSubscriber<>);
 
-			var subscriberTypes = typeResolver.ForeachInAllAssemblies(
-					(type) =>
-					{
-						if (!type.IsClass
-						|| type.IsNotPublic
-						|| type.IsAbstract
-						|| type.IsInterface
-						|| type.IsEnum)
-						{
-							return false;
-						}
+			//var subscriberTypes = typeResolver.ForeachInAllAssemblies(
+			//		(type) =>
+			//		{
+			//			if (!type.IsClass
+			//			|| type.IsNotPublic
+			//			|| type.IsAbstract
+			//			|| type.IsInterface
+			//			|| type.IsEnum)
+			//			{
+			//				return false;
+			//			}
 
-						var ft = type.GetInterface(eventSubscribeInterfaceType.FullName);
-						return ft != null;
-					}
-				).ToArray();
+			//			var ft = type.GetInterface(eventSubscribeInterfaceType.FullName);
+			//			return ft != null;
+			//		}
+			//	).ToArray();
 
-			foreach (var subscriberType in subscriberTypes)
-			{
-				try
-				{
-					this.Container.Register(subscriberType, subscriberType, ServiceLifetime.PerThread);
-					Logger.Verbouse(Contexts.ThisComponent, Categories.Registration, Events.HandlerTypeWasRegistered.With(subscriberType.AssemblyQualifiedName));
-				}
-				catch (Exception e)
-				{
-					Logger.Exception(Contexts.ThisComponent, Categories.Registration, e);
-				}
-			}
+			//foreach (var subscriberType in subscriberTypes)
+			//{
+			//	try
+			//	{
+			//		this.Container.Register(subscriberType, subscriberType, ServiceLifetime.PerThread);
+			//		Logger.Verbouse(Contexts.ThisComponent, Categories.Registration, Events.HandlerTypeWasRegistered.With(subscriberType.AssemblyQualifiedName));
+			//	}
+			//	catch (Exception e)
+			//	{
+			//		Logger.Exception(Contexts.ThisComponent, Categories.Registration, e);
+			//	}
+			//}
 
 
 			var hostLoader = this.Resolver.Resolve<IServerHostLoader>();
 
-			hostLoader.RegisterTrigger("Running Event System Subscribers", () =>
-			{
-				foreach (var subscriberType in subscriberTypes)
-				{
-					try
-					{
-						eventDispatcher.Subscribe(subscriberType);
-						Logger.Verbouse(Contexts.ThisComponent, Categories.Subscribing, Events.HandlerTypeWasConnected.With(subscriberType.AssemblyQualifiedName));
-					}
-					catch (Exception e)
-					{
-						Logger.Exception(Contexts.ThisComponent, Categories.Registration, e);
-					}
-				}
-			});
+			//hostLoader.RegisterTrigger("Running Event System Subscribers", () =>
+			//{
+			//	foreach (var subscriberType in subscriberTypes)
+			//	{
+			//		try
+			//		{
+			//			eventDispatcher.Subscribe(subscriberType);
+			//			Logger.Verbouse(Contexts.ThisComponent, Categories.Subscribing, Events.HandlerTypeWasConnected.With(subscriberType.AssemblyQualifiedName));
+			//		}
+			//		catch (Exception e)
+			//		{
+			//			Logger.Exception(Contexts.ThisComponent, Categories.Registration, e);
+			//		}
+			//	}
+			//});
 
 			var appConfig = this.Resolver.Resolve<AppServerComponentConfig>();
 
