ef306abee18a5d5737aff785694cae7becc82be0
Grigoriy Serebryakov <sdrnsserver@gmail.com>
2020-07-17 17:16:05 +0300

    Add authenticate mechanism (E-Government) for WebQuery

--- Dev/CoreServices/Atdi.CoreServices.AuthService.IcsmViisp/AuthService.cs
+++ Dev/CoreServices/Atdi.CoreServices.AuthService.IcsmViisp/AuthService.cs
@@ -6,27 +6,125 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Atdi.Platform.Logging;
+using Atdi.Contracts.CoreServices.DataLayer;
+using Atdi.Contracts.LegacyServices.Icsm;
+using Atdi.CoreServices.Identity.Models;
+using Atdi.DataModels.DataConstraint;
+using Atdi.CoreServices.Identity;
+using Atdi.DataModels;
+
 
 namespace Atdi.CoreServices.AuthService.IcsmViisp
 {
 	public class AuthService : IAuthService
 	{
-		private readonly AppServerComponentConfig _config;
+        private readonly IQueryExecutor _queryExecutor;
+        private readonly IDataLayer<IcsmDataOrm> _dataLayer;
+        private readonly AppServerComponentConfig _config;
 		private readonly ILogger _logger;
 		public static readonly string Name = "E-Government";
 
-		public AuthService(AppServerComponentConfig config, ILogger logger)
+		public AuthService(AppServerComponentConfig config, IDataLayer<IcsmDataOrm> dataLayer, IQueryExecutor queryExecutor, ILogger logger)
 		{
 			_config = config;
 			_logger = logger;
-		}
+            _dataLayer = dataLayer;
+            _queryExecutor = queryExecutor;
+        }
 
-		public UserIdentity AuthenticateUser(AuthRedirectionResponse response, IUserTokenProvider tokenProvider)
+
+
+        public UserIdentity AuthenticateUser(AuthRedirectionResponse response, IUserTokenProvider tokenProvider)
 		{
-			// тут код работа с E-Government до момента полученого ISCM User ID
-			/*
-			 *
-			 var query = this._dataLayer.Builder
+
+            var userIdentity = new UserIdentity();
+            var userTokenData = new UserTokenData();
+
+            var dicParameters = ParseQueryString(response.Url);
+            if ((dicParameters!=null) && (dicParameters.Count>0))
+            {
+                if ((dicParameters.ContainsKey("ticket")) && (dicParameters.ContainsKey("customData")))
+                {
+
+                }
+                else
+                {
+                    throw new Exception("Response Url not contains parameters 'ticket' or 'customData'"); 
+                }
+            }
+
+
+            var xmlRequest = SignData.AuthenticationData(this._config.ViispSecretKey, _config.ViispPublicKey, "#uniqueNodeId", this._config.ViispPID, dicParameters["ticket"]);
+            var responseInformationData = SignData.GetResponseAuthenticationData(this._config.ViispServiceUrl, xmlRequest, out string faultstring);
+            if (string.IsNullOrEmpty(faultstring))
+            {
+                if (responseInformationData != null)
+                {
+                    if ((responseInformationData.AuthenticationAttribute.Length > 0) && (responseInformationData.AuthenticationAttribute[0].Attribute == "lt-personal-code"))
+                    {
+                        //string Role = "Provider";
+
+
+
+                        var querySelect = this._dataLayer.Builder
+             .From<USERS>()
+             .Where(c => c.REGIST_NUM, ConditionOperator.Equal, responseInformationData.AuthenticationAttribute[0].Value)
+             .Select(
+                 c => c.CODE,
+                 c => c.CUST_TXT3,
+                 c => c.EMAIL,
+                 c => c.ID,
+                 c => c.NAME,
+                 c => c.REGIST_NUM
+                 )
+             .OrderByAsc(c => c.ID)
+             .OnTop(1);
+
+                        var userData = this._queryExecutor
+              .Fetch(querySelect, reader =>
+              {
+                  if (reader.Read())
+                  {
+                      userIdentity.Id = reader.GetValue(x => x.ID);
+                      userIdentity.Name = reader.GetValue(x => x.NAME);
+                      userTokenData.AuthDate = DateTime.Now;
+                      userTokenData.Id = reader.GetValue(x => x.ID);
+                      userTokenData.UserCode = reader.GetValue(x => x.CODE);
+                      userTokenData.UserId = reader.GetValue(x => x.ID);
+                      userTokenData.UserName = reader.GetValue(x => x.NAME);
+                      userIdentity.UserToken = tokenProvider.CreatUserToken(userTokenData);
+
+                      return true;
+                  }
+                  else
+                  {
+                      return false;
+                  }
+
+              });
+
+                        if (userData == false)
+                        {
+
+                            /*
+                            var queryInsert = this._dataLayer.Builder
+                 .Insert("USERS")
+                 .SetValue(new IntegerColumnValue() { Name = "ID", Value = 1 });
+                 */
+                        };
+
+
+                    }
+                }
+            }
+
+
+
+
+            // тут код работа с E-Government до момента полученого ISCM User ID
+            /*
+             *
+             var query = this._dataLayer.Builder
                 .From<EMPLOYEE>()
                 .Where(c => c.WEB_LOGIN, ConditionOperator.Equal, credential.UserName)
                 .Select(
@@ -66,23 +164,23 @@ namespace Atdi.CoreServices.AuthService.IcsmViisp
             if ((string.IsNullOrEmpty(userData.Password) && string.IsNullOrEmpty(credential.Password))
                 || userData.Password == _tokenProvider.GetHashPassword(credential.Password))
             {
-				var tokenData = new UserTokenData
-	            {
-	                AuthDate = DateTime.Now,
-	                Id = user.Id,
-	                UserName = user.WebLogin,
-	                UserCode = user.AppUser,
-	                UserId = user.UserId
-	            };
+                var tokenData = new UserTokenData
+                {
+                    AuthDate = DateTime.Now,
+                    Id = user.Id,
+                    UserName = user.WebLogin,
+                    UserCode = user.AppUser,
+                    UserId = user.UserId
+                };
 
-	            var userToken = this._tokenProvider.CreatUserToken(tokenData);
+                var userToken = this._tokenProvider.CreatUserToken(tokenData);
 
-	            return new UserIdentity
-	            {
-	                Id = user.Id,
-	                Name = user.WebLogin,
-	                UserToken = userToken
-	            };
+                return new UserIdentity
+                {
+                    Id = user.Id,
+                    Name = user.WebLogin,
+                    UserToken = userToken
+                };
 
                 var userIdentity = this.CreateUserIdentity(userData);
                 return userIdentity;
@@ -91,33 +189,58 @@ namespace Atdi.CoreServices.AuthService.IcsmViisp
 
             throw new InvalidOperationException(Exceptions.InvalidUserPassword.With(credential.UserName));
 
-			 *
-			 *
-			 */
-			// обрабатываем пармтеры ответного запроса в нутри которог отикетпользоватлея
-			// по тикету берем даннвй
-			// находим юзера в БД
-			// возвращаем объект
-			return new UserIdentity
-			{
-				UserToken = tokenProvider.CreatUserToken(
-					new UserTokenData
-					{
-						AuthDate =  DateTime.Now,
-						
-					})
-			};
+             *
+             *
+             */
+            // обрабатываем пармтеры ответного запроса в нутри которог отикетпользоватлея
+            // по тикету берем даннвй
+            // находим юзера в БД
+            // возвращаем объект
+            return userIdentity;
 		}
 
-		public AuthRedirectionQuery PrepareAuthRedirection(AuthRedirectionOptions options)
+        public static Dictionary<string, string> ParseQueryString(String query)
+        {
+            var queryDict = new Dictionary<string, string>();
+            var idx = query.TrimStart().LastIndexOf('?');
+            if (idx > 0)
+            {
+                query = query.Remove(0, idx + 1);
+                foreach (String token in query.Split(new char[] { '&' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    string[] parts = token.Split(new char[] { '=' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (parts.Length == 2)
+                    {
+                        queryDict[parts[0].Trim()] = parts[1].Trim();
+                    }
+                    else
+                    {
+                        queryDict[parts[0].Trim()] = "";
+                    }
+                }
+            }
+            return queryDict;
+        }
+
+        public AuthRedirectionQuery PrepareAuthRedirection(AuthRedirectionOptions options)
 		{
-			// Тут начал апроцесса, первая фаза до формирвоания урла для ридеректа
-			// его формируем ми перадем в свойствах объекта  результата
-			return new AuthRedirectionQuery
-			{
-				AuthService = options.AuthService,
-				Url = ""
-			};
+            // Тут начал апроцесса, первая фаза до формирвоания урла для ридеректа
+            // его формируем ми перадем в свойствах объекта  результата
+            var correlationData = Guid.NewGuid();
+            var xmlInit = SignData.InitAuthentication(this._config.ViispSecretKey, _config.ViispPublicKey, "#uniqueNodeId", this._config.ViispPID, options.ReturnUrl, correlationData.ToString());
+            var ticketId = SignData.GetResponseinitAuthentication(this._config.ViispServiceUrl, xmlInit, out string faultString);
+            if (string.IsNullOrEmpty(faultString))
+            {
+                return new AuthRedirectionQuery
+                {
+                    AuthService = options.AuthService,
+                    Url = this._config.ViispServiceUrl + string.Format("?ticket={0}", ticketId)
+                };
+            }
+            else
+            {
+                throw new Exception(faultString);
+            }
 		}
 	}
 }
---
+++ Dev/CoreServices/Atdi.CoreServices.AuthService.IcsmViisp/Crypto/Helpers.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Atdi.CoreServices.AuthService.IcsmViisp
+{
+    public class Helpers
+    {
+        public static int DecodeIntegerSize(System.IO.BinaryReader rd)
+        {
+            byte byteValue;
+            int count;
+
+            byteValue = rd.ReadByte();
+            if (byteValue != 0x02)        
+                return 0;
+
+            byteValue = rd.ReadByte();
+            if (byteValue == 0x81)
+            {
+                count = rd.ReadByte();    
+            }
+            else if (byteValue == 0x82)
+            {
+                byte hi = rd.ReadByte();  
+                byte lo = rd.ReadByte();
+                count = BitConverter.ToUInt16(new[] { lo, hi }, 0);
+            }
+            else
+            {
+                count = byteValue;        
+            }
+
+            while (rd.ReadByte() == 0x00)
+            {
+                count -= 1;
+            }
+            rd.BaseStream.Seek(-1, System.IO.SeekOrigin.Current);
+
+            return count;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pemString"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static byte[] GetBytesFromPEM(string pemString, PemStringType type)
+        {
+            string header; string footer;
+
+            switch (type)
+            {
+                case PemStringType.Certificate:
+                    header = "-----BEGIN CERTIFICATE-----";
+                    footer = "-----END CERTIFICATE-----";
+                    break;
+                case PemStringType.RsaPrivateKey:
+                    header = "-----BEGIN RSA PRIVATE KEY-----";
+                    footer = "-----END RSA PRIVATE KEY-----";
+                    break;
+                default:
+                    return null;
+            }
+
+            int start = pemString.IndexOf(header) + header.Length;
+            int end = pemString.IndexOf(footer, start) - start;
+            return Convert.FromBase64String(pemString.Substring(start, end));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="inputBytes"></param>
+        /// <param name="alignSize"></param>
+        /// <returns></returns>
+        public static byte[] AlignBytes(byte[] inputBytes, int alignSize)
+        {
+            int    inputBytesSize = inputBytes.Length;
+
+            if ((alignSize != -1) && (inputBytesSize < alignSize))
+            {
+                byte[] buf = new byte[alignSize];
+                for (int i = 0; i < inputBytesSize; ++i)
+                {
+                    buf[i + (alignSize - inputBytesSize)] = inputBytes[i];
+                }
+                return buf;
+            }
+            else
+            {
+                return inputBytes;      
+            }
+        }
+    }
+}
