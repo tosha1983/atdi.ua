973b0cbd1a2de094a3d8a2b65ba1bc3754d21118
Andrey Kovpak <kovpak@outlook.com>
2020-07-17 11:02:05 +0300

    Added external  Auth Services

---
+++ Dev/Contracts/CoreServices/Atdi.Contracts.CoreServices.Identity/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using Atdi.DataModels.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atdi.Contracts.CoreServices.Identity
+{
+	public interface IAuthService
+	{
+		AuthRedirectionQuery PrepareAuthRedirection(AuthRedirectionOptions options);
+
+		UserIdentity AuthenticateUser(AuthRedirectionResponse response, IUserTokenProvider tokenProvider);
+	}
+}
