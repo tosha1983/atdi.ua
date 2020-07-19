37958da8d92e2a614b6d46d45c8d16f7b1bb82cc
Andrey Kovpak <kovpak@outlook.com>
2020-07-19 20:38:58 +0300

    Updated WebQuery

--- Dev/WebPortals/Atdi.WebPortal.WebQuery/ClientApp/Components/FooterBar.vue
+++ Dev/WebPortals/Atdi.WebPortal.WebQuery/ClientApp/Components/FooterBar.vue
@@ -2,7 +2,7 @@
     <footer class="page-footer portal-footer z-depth-4">
         <div class="footer-copyright">
             <div class="container">
-                Developed by L.I.S Ukraine © {{(new Date()).getFullYear()}}
+                Developed by ATDI Ukraine © {{(new Date()).getFullYear()}}
                 <a class="grey-text text-lighten-4 right" :href="'//' + site" target="_blank">{{title}}</a>
             </div>
         </div>
