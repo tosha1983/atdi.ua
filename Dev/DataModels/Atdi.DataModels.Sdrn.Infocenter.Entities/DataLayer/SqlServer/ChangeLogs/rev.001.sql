25ce518d9ffbfd8d525fad2dc13d4377a8bf3f31
Andrey Kovpak <kovpak@outlook.com>
2020-08-05 14:05:41 +0300

    Added Entity of Sensor to infocenter

--- Dev/DataModels/Atdi.DataModels.Sdrn.Infocenter.Entities/DataLayer/SqlServer/ChangeLogs/rev.001.sql
+++ Dev/DataModels/Atdi.DataModels.Sdrn.Infocenter.Entities/DataLayer/SqlServer/ChangeLogs/rev.001.sql
@@ -1,1 +1,16 @@
-﻿
+﻿create table [SDRNSVR].[SENSOR] (
+	[ID] 	[bigint]  not null,
+	[SENSORIDENTIFIER_ID]	[bigint] null,
+	[STATUS]	[nvarchar](25) null,
+	[NAME]	[nvarchar](150) null,
+	[BIUSEDATE]	[datetime] null,
+	[EOUSEDATE]	[datetime] null,
+	[AZIMUTH]	[float] null,
+	[ELEVATION]	[float] null,
+	[AGL]	[float] null,
+	[RXLOSS]	[float] null,
+	[TECHID]	[nvarchar](150) null,
+	constraint [PK_SENSOR] primary key clustered ([ID]) on [PRIMARY]  
+) on [PRIMARY]
+
+GO
---
+++ Dev/DataModels/Atdi.DataModels.Sdrn.Infocenter.Entities/Entities/SdrnServer/ISensor.cs
@@ -0,0 +1,39 @@
+﻿using Atdi.DataModels.EntityOrm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atdi.DataModels.Sdrn.Infocenter.Entities.SdrnServer
+{
+	[EntityPrimaryKey]
+	public interface ISensor_PK
+	{
+		long Id { get; set; }
+	}
+
+	[Entity]
+	public interface ISensor : ISensor_PK
+	{
+		long? SensorIdentifierId { get; set; }
+
+		string Status { get; set; }
+
+		string Name { get; set; }
+
+		DateTime? BiuseDate { get; set; }
+
+		DateTime? EouseDate { get; set; }
+
+		double? Azimuth { get; set; }
+
+		double? Elevation { get; set; }
+
+		double? Agl { get; set; }
+
+		double? RxLoss { get; set; }
+
+		string TechId { get; set; }
+	}
+}
