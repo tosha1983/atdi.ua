c3f122172212732c6239d1e447913ffb390fb5f5
Grigoriy Serebryakov <sdrnsserver@gmail.com>
2020-08-05 23:25:19 +0300

    RefSpectrum task Intermediate changes

--- Dev/DataModels/Atdi.DataModels.Sdrn.CalcServer.Internal/Iterations/RefSpectrumByDriveTestsCalc/CalcRefSpectrum/Output/ResultRefSpectrumByDriveTests.cs
+++ Dev/DataModels/Atdi.DataModels.Sdrn.CalcServer.Internal/Iterations/RefSpectrumByDriveTestsCalc/CalcRefSpectrum/Output/ResultRefSpectrumByDriveTests.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Atdi.DataModels.Sdrn.DeepServices.Gis;
-using Atdi.DataModels.Sdrn.DeepServices.RadioSystem.Stations;
+
 
 namespace Atdi.DataModels.Sdrn.CalcServer.Internal.Iterations
 {
