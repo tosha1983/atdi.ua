0fad8257cb89604654f6f217e9f61879dbb47b5d
Grigoriy Serebryakov <sdrnsserver@gmail.com>
2020-08-18 17:02:29 +0300

    Check  RefSpectrum

--- Dev/AppUnits/Atdi.AppUnits.Sdrn.CalcServer.Tasks/Tasks/RefSpectrum/RefSpectrumByDriveTestsCalcTask.cs
+++ Dev/AppUnits/Atdi.AppUnits.Sdrn.CalcServer.Tasks/Tasks/RefSpectrum/RefSpectrumByDriveTestsCalcTask.cs
@@ -234,7 +234,11 @@ namespace Atdi.AppUnits.Sdrn.CalcServer.Tasks
                     for (int j = 0; j < freqs_MHz.Length; j++)
                     {
                         var idSensor = FindSensor(freqs_MHz[j], sensor.SensorAntennas);
-   
+                        if (idSensor==null)
+                        {
+                            continue;
+                        }
+                        var fndSensorAntennas = sensor.SensorAntennas[idSensor.Value];
 
                         var lstReceivedPowerCalcResult = new List<ReceivedPowerCalcResult>();
                         for (int k = 0; k < this._refSpectrumStationCalibrations.Length; k++)
@@ -343,11 +347,7 @@ namespace Atdi.AppUnits.Sdrn.CalcServer.Tasks
                         {
                             for (int n = 0; n < percentTimeForGainCalcData.StationData.Length; n++)
                             {
-                                var fndStationData = listResultRefSpectrumBySensors.Find(x =>  Math.Round(x.Freq_MHz,6) == Math.Round(percentTimeForGainCalcData.StationData[n].Frequency_Mhz.Value,6));
-                                if (fndStationData != null)
-                                {
-                                    fndStationData.Percent = resultPercent[n];
-                                }
+                                listResultRefSpectrumBySensors[n].Percent = resultPercent[n];
                             }
                         }
                     }
