19ac5922b4dc2e277107f908e2dc94795baaa0bf
Grisha <sdrnsserver@gmail.com>
2020-09-28 15:57:22 +0300

    Check Atdi.Test.CalcServer.LowFunction

---
+++ Dev/Test/Atdi.Test.CalcServer.LowFunction/Test/Coordinated.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atdi.Test.CalcServer.LowFunction
+{
+
+    public struct Coordinated
+    {
+       public  double Lon_DEC;
+        public double Lat_DEC;
+        public double Level_dBm;
+    }
+}
